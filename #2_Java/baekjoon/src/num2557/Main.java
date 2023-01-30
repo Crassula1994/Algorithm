bf325107cce098a9bba2f0adc6a3f84696833bcb
Crassula1994 <gogun3535@gmail.com>
2023-01-30 14:30:31 +0900

    Algorithm Repository Created

---
+++ #2_Java/baekjoon/src/num2557/Main.java
@@ -0,0 +1,12 @@
+package num2557;
+
+// Main 클래스 정의
+public class Main {
+ 
+    // main() 메서드 정의
+    public static void main(String[] args) {
+     
+    	// 'Hello World!' 출력
+    	System.out.println("Hello World!");
+    }
+}
