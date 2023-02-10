ca154e2044e35bde745bb82bffe83c288c27f0db
Crassula <gogun3535@gmail.com>
2023-02-10 23:48:19 +0900

    Beakjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num11942/Main.java
@@ -0,0 +1,12 @@
+package bronze5.num11942;
+
+// Main 클래스 정의
+public class Main {
+ 
+    // main() 메서드 정의
+    public static void main(String[] args) {
+     
+    	// '고려대학교' 출력
+    	System.out.println("고려대학교");
+    }
+}
