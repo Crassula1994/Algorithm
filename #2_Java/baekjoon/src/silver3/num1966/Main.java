9340cd58e3db174ee70e312d0e6e081b44c610cf
Crassula <gogun3535@gmail.com>
2023-02-14 21:18:46 +0900

    Beakjoon Java Solution Modified

--- #2_Java/baekjoon/src/silver3/num1966/Main.java
+++ #2_Java/baekjoon/src/silver3/num1966/Main.java
@@ -6,8 +6,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -39,11 +39,7 @@ public class Main {
 			
 			// 각 문서의 인쇄 순서를 담을 Queue 객체 printQueue 초기화
 			Queue<int[]> printQueue = new LinkedList<>();
-			
-			(dc1 , dc2) -> {
-				return dc1[1] != dc2[1] ? dc2[1] - dc1[1] : dc1[0] - dc2[0];
-			}
-			
+
 			// 해당 문서가 출력된 순서를 저장할 변수 order 초기화
 			int order = 0;
 			
@@ -60,11 +56,46 @@ public class Main {
 			// while 반복문을 사용해 해당 문서가 출력될 때까지 순회
 			while (true) {
 				
+				// iterator() 메서드를 사용해 Iterator 객체를 priorityChecker 변수에 할당
+				Iterator<int[]> priorityChecker = printQueue.iterator();
+				
+				// 현재 최대 중요도 및 해당 인덱스를 저장할 각 변수 초기화
+				int topPriority = 0;
+				int topIdx = -1;
+				
+				// 현재 순회 중인 인덱스를 저장할 변수 index 초기화
+				int index = -1;
+				
+				// while 반복문을 사용해 printQueue의 각 원소를 차례로 순회
+				while (priorityChecker.hasNext()) {
+				
+					// 현재 순회 중인 인덱스 갱신
+					index++;
+					
+					// next() 메서드를 사용해 해당 원소의 값을 배열 document에 할당
+					int[] document = priorityChecker.next();
+					
+					// 중요도가 현재 최대 중요도보다 높은 경우 최대 중요도 갱신
+					if (document[1] > topPriority) {
+						topPriority = document[1];
+						topIdx = index;
+					}
+				}
+				
+				// for 반복문을 사용해 최대 중요도를 지닌 값의 전까지 순회
+				for (int idx = 0; idx < topIdx; idx++)
+					
+					// poll() 및 offer() 메서드를 사용해 우선순위가 낮은 문서를 뒤로 재배치
+					printQueue.offer(printQueue.poll());
+	
+				// poll() 메서드를 사용해 가장 우선순위가 높은 문서를 뽑아 변수 printedDoc에 할당
+				int[] printedDoc = printQueue.poll();
+				
 				// 문서가 출력된 순서 갱신
 				order++;
 				
 				// 목표로 하던 문서가 출력된 경우 반복문 탈출
-				if (printQueue.poll()[0] == targetDoc)
+				if (printedDoc[0] == targetDoc)
 					break;
 			}
 			
