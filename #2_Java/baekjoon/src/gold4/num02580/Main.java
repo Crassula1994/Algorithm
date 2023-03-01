1503d9598867dc7c221571b956b0f2bccbd0c261
Crassula <gogun3535@gmail.com>
2023-03-01 20:18:19 +0900

    Beakjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold4/num02580/Main.java
+++ #2_Java/baekjoon/src/gold4/num02580/Main.java
@@ -6,8 +6,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 // Main 클래스 정의
@@ -25,9 +23,6 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		// 빈 칸의 위치를 저장할 Queue 객체 blankLoc 초기화
-		Queue<int[]> blankLoc = new LinkedList<>();
-		
 		// for 반복문을 사용해 스도쿠 판의 각 행을 순회
 		for (int r = 0; r < 9; r++) {
 			
@@ -35,46 +30,12 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(in.readLine());
 			
 			// for 반복문을 사용해 스도쿠 판의 각 열을 순회하며 스도쿠 판에 값을 저장
-			for (int c = 0; c < 9; c++) {
+			for (int c = 0; c < 9; c++)
 				sudoku[r][c] = Integer.parseInt(st.nextToken());
-				
-				// 빈 칸인 경우 해당 위치를 blankLoc에 추가
-				if (sudoku[r][c] == 0) {
-					int[] location = {r, c};
-					blankLoc.offer(location);
-				}
-			}
-		}
-		
-		// while() 반복문을 사용해 빈 칸이 모두 없어질 때까지 순회
-		while (!blankLoc.isEmpty()) {
-			
-			// poll() 메서드를 사용해 빈 칸의 위치를 배열 location에 할당
-			int[] location = blankLoc.poll();
-			
-			// rowChecker() 메서드를 실행해 답을 구할 수 있는 경우 다음 빈 칸을 순회
-			if (rowChecker(location[0], location[1])) continue;
-			
-			// columnChecker() 메서드를 실행해 답을 구할 수 있는 경우 다음 빈 칸을 순회
-			if (columnChecker(location[0], location[1])) continue;
-			
-			// boxChecker() 메서드를 실행해 답을 구할 수 있는 경우 다음 빈 칸을 순회
-			if (boxChecker(location[0], location[1])) continue;
-			
-			// offer() 메서드를 사용해 해당 값을 채울 수 없는 경우 blankLoc에 다시 추가
-			blankLoc.offer(location);
 		}
 
-		// for 반복문을 사용해 스도쿠 판의 각 행을 순회
-		for (int r = 0; r < 9; r++) {
-			
-			// for 반복문을 사용해 스도쿠 판의 각 열을 순회하며 값을 출력
-			for (int c = 0; c < 9; c++)
-				out.write(sudoku[r][c] + " ");
-			
-			// write() 메서드를 사용해 줄바꿈을 출력
-			out.write("\n");
-		}
+		// sudokuMaker() 메서드를 호출해 가능한 정답을 출력
+		sudokuMaker(0, 0, out);
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
@@ -83,59 +44,99 @@ public class Main {
 	
 	// ----------------------------------------------------------------------------------------------------
 	
-	// rowChecker() 메서드 정의
-	public static boolean rowChecker(int row, int column) {
+	// sudokuMaker() 메서드 정의
+	public static void sudokuMaker(int row, int column, BufferedWriter out) throws IOException {
 		
-		// 스도쿠의 빈 칸의 정답을 저장할 변수 answer 초기화
-		int answer = 45;
+		// 해당 행을 완성한 경우 다음 행을 검사할 수 있도록 sudokuMaker() 메서드 재귀 호출 후 함수 종료
+		if (column == 9) {
+			sudokuMaker(row + 1, 0, out);
+			return;
+		}
+		
+		// 스도쿠를 모두 완성한 경우
+		if (row == 9) {
+			
+			// for 반복문을 사용해 스도쿠 판의 각 행을 순회
+			for (int r = 0; r < 9; r++) {
+				
+				// for 반복문을 사용해 스도쿠 판의 각 열을 순회하며 값을 출력
+				for (int c = 0; c < 9; c++)
+					out.write(sudoku[r][c] + " ");
+				
+				// newLine() 메서드를 사용해 줄바꿈을 출력
+				out.newLine();
+			}
+			
+			// flush() 메서드를 사용해 지금까지의 내용을 출력
+			out.flush();
+			
+			// exit() 메서드를 사용해 JVM 정상 종료
+			System.exit(0);
+		}
+		
+		// 해당 칸이 0인 경우
+		if (sudoku[row][column] == 0) {
+			
+			// for 반복문을 사용해 1부터 9까지의 숫자 중 가능한 숫자를 순회
+			for (int num = 1; num <= 9; num++) {
+				
+				// 해당 숫자를 넣을 수 있는 경우
+				if (rowChecker(row, column, num) && colChecker(row, column, num) && boxChecker(row, column, num)) {
+					
+					// 해당 숫자를 빈 칸에 갱신 후 sudokuMaker() 메서드 재귀 호출
+					sudoku[row][column] = num;
+					sudokuMaker(row, column + 1, out);
+				}
+			}
+			
+			// 1부터 9까지 넣어보아도 답을 구성할 수 없는 경우 값 초기화 후 함수 종료
+			sudoku[row][column] = 0;
+			return;
+			
+		// 해당 칸이 0이 아닌 경우 다음 칸을 검사하도록 sudokuMaker() 메서드 재귀 호출
+		} else {
+			sudokuMaker(row, column + 1, out);
+		}
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+	
+	// rowChecker() 메서드 정의
+	public static boolean rowChecker(int row, int column, int possibleNum) {
 		
 		// for 반복문을 사용해 해당 행을 차례로 순회
 		for (int c = 0; c < 9; c++) {
 			
-			// 해당 행에 또 다른 0이 있을 경우 false 반환
-			if (c != column && sudoku[row][c] == 0)
+			// 해당 행에 또 다른 possibleNum이 있을 경우 false 반환
+			if (sudoku[row][c] == possibleNum)
 				return false;
-			
-			// 해당 값을 갱신
-			answer -= sudoku[row][c];
 		}
 		
-		// 정답을 찾을 수 있는 경우 빈 칸을 채우고 true 반환
-		sudoku[row][column] = answer;
+		// possibleNum을 넣을 수 있는 경우 true 반환
 		return true;
 	}
 	
 	// ----------------------------------------------------------------------------------------------------
 	
-	// columnChecker() 메서드 정의
-	public static boolean columnChecker(int row, int column) {
-		
-		// 스도쿠의 빈 칸의 정답을 저장할 변수 answer 초기화
-		int answer = 45;
+	// colChecker() 메서드 정의
+	public static boolean colChecker(int row, int column, int possibleNum) {
 		
 		// for 반복문을 사용해 해당 열을 차례로 순회
 		for (int r = 0; r < 9; r++) {
 			
-			// 해당 행에 또 다른 0이 있을 경우 false 반환
-			if (r != row && sudoku[r][column] == 0)
+			// 해당 열에 또 다른 possibleNum이 있을 경우 false 반환
+			if (sudoku[r][column] == possibleNum)
 				return false;
-			
-			// 해당 값을 갱신
-			answer -= sudoku[r][column];
 		}
 		
-		// 정답을 찾을 수 있는 경우 빈 칸을 채우고 true 반환
-		sudoku[row][column] = answer;
+		// possibleNum을 넣을 수 있는 경우 true 반환
 		return true;
 	}
 	
 	// ----------------------------------------------------------------------------------------------------
 	
 	// boxChecker() 메서드 정의
-	public static boolean boxChecker(int row, int column) {
-		
-		// 스도쿠의 빈 칸의 정답을 저장할 변수 answer 초기화
-		int answer = 45;
+	public static boolean boxChecker(int row, int column, int possibleNum) {
 		
 		// 박스의 시작점의 각 좌표를 계산해 각 변수에 할당
 		int startRow = 3 * (row / 3);
@@ -145,17 +146,13 @@ public class Main {
 		for (int r = startRow; r < startRow + 3; r++) {
 			for (int c = startCol; c < startCol + 3; c++) {
 				
-				// 해당 박스에 또 다른 0이 있을 경우 false 반환
-				if ((r != row || c != column) && sudoku[r][c] == 0)
+				// 해당 박스에 또 다른 possibleNum이 있을 경우 false 반환
+				if (sudoku[r][c] == possibleNum)
 					return false;
-				
-				// 해당 값을 갱신
-				answer -= sudoku[r][c];
 			}
 		}
 		
-		// 정답을 찾을 수 있는 경우 빈 칸을 채우고 true 반환
-		sudoku[row][column] = answer;
+		// possibleNum을 넣을 수 있는 경우 true 반환
 		return true;
 	}
 }
--- #2_Java/baekjoon/src/silver2/num01260/Main.java
+++ #2_Java/baekjoon/src/silver2/num01260/Main.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // Main 클래스 정의
@@ -55,14 +57,75 @@ public class Main {
 			connection[fromNode][toNode] = true;
 		}
 		
-		// dfs 함수를 호출해 dfs를 수행한 결과를 출력
+		// dfs 함수를 호출해 깊이 우선 탐색(DFS)을 수행한 결과를 출력
+		dfs(startNode, out);
 		
+		// 각 정점의 방문 여부를 저장할 배열 visited 초기화
+		visited = new boolean[nodeNum + 1];
 		
-		// write() 메서드를 사용해 최대공약수 및 최소공배수를 출력
-		out.write(gcd + "\n" + lcm);
+		// newLine() 메서드를 사용해 줄바꿈 출력
+		out.newLine();
+		
+		// bfs 함수를 호출해 너비 우선 탐색(BFS)을 수행한 결과를 출력
+		bfs(startNode, out);
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
 		out.close();
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+	
+	// dfs() 메서드 정의
+	public static void dfs(int startNode, BufferedWriter out) throws IOException {
+		
+		// 입력 받은 노드를 방문 처리
+		visited[startNode] = true;
+		
+		// write() 메서드를 사용해 방문 처리한 노드를 출력
+		out.write(startNode + " ");
+		
+		// for 반복문을 사용해 다른 노드를 순회
+		for (int node = 1; node < connection.length; node++) {
+			
+			// 해당 노드와 연결되어 있고 방문한 노드가 아닐 경우 dfs() 메서드 재귀 호출
+			if (connection[startNode][node] && !visited[node])
+				dfs(node, out);
+		}
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+	
+	// bfs() 메서드 정의
+	public static void bfs(int startNode, BufferedWriter out) throws IOException {
+		
+		// 방문할 다음 위치를 저장할 Queue 객체 nodeQueue 초기화
+		Queue<Integer> nodeQueue = new LinkedList<>();
+		
+		// offer() 메서드를 사용해 입력 받은 노드를 nodeQueue에 추가
+		nodeQueue.offer(startNode);
+		
+		// 해당 노드 방문 처리
+		visited[startNode] = true;
+		
+		// while 문을 사용해 nodeQueue가 빌 때까지 순회
+		while (!nodeQueue.isEmpty()) {
+			
+			// poll() 메서드를 사용해 현재 방문한 노드를 꺼내와 변수 currentNode에 할당
+			int currentNode = nodeQueue.poll();
+			
+			// write() 메서드를 사용해 현재 노드를 출력
+			out.write(currentNode + " ");
+			
+			// for 반복문을 사용해 다른 노드를 순회
+			for (int node = 1; node < connection.length; node++) {
+				
+				// 해당 노드와 연결되어 있고 방문한 노드가 아닐 경우 nodeQueue에 추가 및 방문 처리
+				if (connection[currentNode][node] && !visited[node]) {
+					nodeQueue.offer(node);
+					visited[node] = true;
+				}
+			}
+		}
 	}
 }
