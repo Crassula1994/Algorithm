d6d1fdd76ef6cf2ed611b61ba9abc733b121ba22
gogun3535 <gogun3535@gmail.com>
2023-03-06 11:46:27 +0900

    Baekjoon Java Solution Added

--- #2_Java/baekjoon/src/gold5/num07569/Main.java
+++ #2_Java/baekjoon/src/gold5/num07569/Main.java
@@ -1,4 +1,4 @@
-package gold5.num07576;
+package gold5.num07569;
 
 // 필요한 패키지 불러오기
 import java.io.BufferedReader;
@@ -13,19 +13,21 @@ import java.util.StringTokenizer;
 // Main 클래스 정의
 public class Main {
 	
-	// 토마토의 보관 상태를 저장할 2차원 배열 warehouse 초기화
-	static int[][] warehouse;
+	// 토마토의 보관 상태를 저장할 3차원 배열 warehouse 초기화
+	static int[][][] warehouse;
 	
-	// 창고의 가로와 세로의 크기를 나타낼 각 변수 초기화
+	// 창고의 가로와 세로의 크기 및 높이를 나타낼 각 변수 초기화
 	static int width;
+	static int depth;
 	static int height;
 	
 	// 차례로 탐색할 위치를 저장할 Queue 객체 visitList 초기화
 	static Queue<int[]> visitList = new LinkedList<>();
 	
 	// 토마토의 익는 방향을 저장할 각 델타 배열 초기화
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};	
+	static int[] dh = {0, 0, 0, 0, -1, 1};
+	static int[] dr = {-1, 1, 0, 0, 0 ,0};
+	static int[] dc = {0, 0, -1, 1, 0, 0};
 	
 	// ----------------------------------------------------------------------------------------------------
 	
@@ -39,34 +41,39 @@ public class Main {
 		// StringTokenizer 객체를 불러와 변수 st에 할당
 		StringTokenizer st  = new StringTokenizer(in.readLine());
 		
-		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 창고의 가로와 세로 칸 수를 각 변수에 할당
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 창고의 가로와 세로 칸 수 및 높이를 각 변수에 할당
 		width = Integer.parseInt(st.nextToken());
+		depth = Integer.parseInt(st.nextToken());
 		height = Integer.parseInt(st.nextToken());
 		
 		// 토마토의 보관 상태를 저장할 2차원 배열 warehouse 초기화
-		warehouse = new int[height][width];
+		warehouse = new int[height][depth][width];
 		
 		// 토마토가 모두 익었는지 검사할 변수 isRipe 초기화
 		boolean isRipe = true;
 
-		// for 반복문을 사용해 창고의 행을 순회
-		for (int r = 0; r < height; r++) {
+		// for 반복문을 사용해 창고의 각 층을 순회
+		for (int h = 0; h < height; h++) {
 			
-			// StringTokenizer 객체를 불러와 변수 st에 재할당
-			st  = new StringTokenizer(in.readLine());
+			// for 반복문을 사용해 창고의 행을 순회
+			for (int r = 0; r < depth; r++) {
 			
-			// for 반복문을 사용해 창고의 열을 순회하며 배열 warehouse에 토마토의 상태 저장
-			for (int c = 0; c < width; c++) {
-				warehouse[r][c] = Integer.parseInt(st.nextToken());
+				// StringTokenizer 객체를 불러와 변수 st에 재할당
+				st  = new StringTokenizer(in.readLine());
 				
-				// 토마토가 하나라도 익지 않은 경우 isRipe 변수 갱신
-				if (isRipe && warehouse[r][c] == 0)
-					isRipe = false;
-				
-				// 토마토가 익은 경우 해당 위치를 visitList에 추가
-				if (warehouse[r][c] == 1) {
-					int[] location = {r , c};
-					visitList.add(location);
+				// for 반복문을 사용해 창고의 열을 순회하며 배열 warehouse에 토마토의 상태 저장
+				for (int c = 0; c < width; c++) {
+					warehouse[h][r][c] = Integer.parseInt(st.nextToken());
+					
+					// 토마토가 하나라도 익지 않은 경우 isRipe 변수 갱신
+					if (isRipe && warehouse[h][r][c] == 0)
+						isRipe = false;
+					
+					// 토마토가 익은 경우 해당 위치를 visitList에 추가
+					if (warehouse[h][r][c] == 1) {
+						int[] location = {h, r , c};
+						visitList.add(location);
+					}
 				}
 			}
 		}
@@ -102,21 +109,22 @@ public class Main {
 			int[] currentLoc = visitList.poll();
 			
 			// for 반복문을 사용해 현재 위치에 인접한 위치를 순회
-			for (int d = 0; d < 4; d++) {
+			for (int d = 0; d < 6; d++) {
 				
 				// 인접한 위치를 각 변수에 할당
-				int nr = currentLoc[0] + dr[d];
-				int nc = currentLoc[1] + dc[d];
+				int nh = currentLoc[0] + dh[d];
+				int nr = currentLoc[1] + dr[d];
+				int nc = currentLoc[2] + dc[d];
 				
 				// 인접한 위치가 배열을 벗어나지 않고 익지 않은 경우
-				if (nr >= 0 && nr < height && nc >= 0 && nc < width && warehouse[nr][nc] == 0) {
+				if (nh >= 0 && nh < height && nr >= 0 && nr < depth && nc >= 0 && nc < width && warehouse[nh][nr][nc] == 0) {
 					
 					// 해당 위치를 visitList에 추가
-					int[] location = {nr , nc};
-					visitList.add(location);
+					int[] nextLoc = {nh, nr , nc};
+					visitList.add(nextLoc);
 					
 					// 익은 날짜를 갱신
-					warehouse[nr][nc] = warehouse[currentLoc[0]][currentLoc[1]] + 1;
+					warehouse[nh][nr][nc] = warehouse[currentLoc[0]][currentLoc[1]][currentLoc[2]] + 1;
 				}
 			}
 		}
@@ -124,16 +132,18 @@ public class Main {
 		// 토마토가 익는 데 필요한 시간을 저장할 변수 minTime 초기화
 		int minTime = 0;
 		
-		// for 반복문을 사용해 토마토 창고의 행과 열을 순회
-		for (int r = 0; r < height; r++) {
-			for (int c = 0; c < width; c++) {
-				
-				// 익지 않은 토마토가 있는 경우 -1을 반환
-				if (warehouse[r][c] == 0)
-					return -1;
-				
-				// 토마토가 익는 데 필요한 시간을 갱신
-				minTime = (warehouse[r][c] > minTime) ? warehouse[r][c] : minTime;
+		// for 반복문을 사용해 토마토 창고의 높이, 행과 열을 순회
+		for (int h = 0; h < height; h++) {
+			for (int r = 0; r < depth; r++) {
+				for (int c = 0; c < width; c++) {
+					
+					// 익지 않은 토마토가 있는 경우 -1을 반환
+					if (warehouse[h][r][c] == 0)
+						return -1;
+					
+					// 토마토가 익는 데 필요한 시간을 갱신
+					minTime = (warehouse[h][r][c] > minTime) ? warehouse[h][r][c] : minTime;
+				}
 			}
 		}
 		
