58eee1834060e3dec48bbf17487f5f2c9d95fde6
Crassula <gogun3535@gmail.com>
2023-03-06 22:35:04 +0900

    Beakjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold4/num01715/Main.java
+++ #2_Java/baekjoon/src/gold4/num01715/Main.java
@@ -22,26 +22,34 @@ public class Main {
 		int cardDecks = Integer.parseInt(in.readLine());
 		
 		// 각 카드 묶음의 수를 저장할 PriorityQueue 객체 minHeap 초기화
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		PriorityQueue<Long> minHeap = new PriorityQueue<>();
 		
 		// for 반복문을 사용해 각 카드 묶음을 순회하며 minHeap에 저장
 		for (int c = 0; c < cardDecks; c++)
-			minHeap.offer(Integer.parseInt(in.readLine()));
+			minHeap.offer(Long.parseLong(in.readLine()));
 		
 		// 최소 비교 횟수를 저장할 변수 minCnt 초기화
-		int minCnt = 0;
+		long minCnt = 0;
 		
 		// 카드 묶음이 두 개 이상인 경우
 		if (cardDecks > 1) {
 			
-			// poll() 메서드를 사용해 카드 묶음 첫 두 개를 합친 값을 갱신
-			minCnt += minHeap.poll() + minHeap.poll();
-		
 			// while() 반복문을 사용해 minHeap이 빌 때까지 순회
-			while (!minHeap.isEmpty())
+			while (true) {
 				
+				// poll() 메서드를 사용해 가장 작은 수의 카드 묶음 두 개를 합친 값을 변수 sum에 할당
+				long sum = minHeap.poll() + minHeap.poll();
+			
 				// 최소 비교 횟수를 갱신
-				minCnt += minCnt + minHeap.poll();
+				minCnt += sum;
+				
+				// minHeap이 빈 경우 반복문 탈출
+				if (minHeap.isEmpty())
+					break;
+				
+				// offer() 메서드를 사용해 합친 카드 묶음을 minHeap에 추가
+				minHeap.offer(sum);
+			}
 		}
 		
 		// valueOf() 및 write() 메서드를 사용해 최소 비교 횟수을 출력
