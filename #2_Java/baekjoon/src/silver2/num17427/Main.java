6c1e30b9503239a6cd883875d661c23850d8a81e
Crassula <gogun3535@gmail.com>
2023-03-08 19:19:10 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/silver2/num17427/Main.java
@@ -0,0 +1,37 @@
+package silver2.num17427;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 숫자를 변수 number에 할당
+		int number = Integer.parseInt(in.readLine());
+		
+		// 입력 받은 숫자의 약수의 합을 저장할 변수 divisorSum 초기화
+		long divisorSum = 0;
+		
+		// for 반복문을 사용해 입력 받은 숫자의 약수의 합을 갱신
+		for (int n = 1; n <= number; n++)
+			divisorSum += n * (number / n);
+			
+		// valueOf() 및 write() 메서드를 사용해 약수의 합을 출력
+		out.write(String.valueOf(divisorSum));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
