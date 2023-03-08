73a34872201c905c69406b68e3cb0796d27760c8
Crassula <gogun3535@gmail.com>
2023-03-09 01:13:12 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num27866/Main.java
@@ -0,0 +1,33 @@
+package bronze5.num27866;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 메서드를 사용해 입력 받은 단어를 변수 word에 할당
+		String word = in.readLine();
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 글자의 위치를 변수 index에 할당
+		int index = Integer.parseInt(in.readLine());
+		
+		// charAt() 및 write() 메서드를 사용해 해당 위치의 글자를 출력
+		out.write(word.charAt(index - 1));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
