dfa9aa427e22dd00f488b3c35bb8c86a26486efe
gogun3535 <gogun3535@gmail.com>
2023-03-10 17:48:42 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num01271/Main.java
@@ -0,0 +1,16 @@
+package bronze5.num01271;
+
+// Main 클래스 정의
+public class Main {
+ 
+    // main() 메서드 정의
+    public static void main(String[] args) {AAAA
+     
+    	// 개 모양 출력
+    	System.out.println("|\\_/|");
+    	System.out.println("|q p|   /}");
+    	System.out.println("( 0 )\"\"\"\\");
+    	System.out.println("|\"^\"`    |");
+    	System.out.println("||_/=\\\\__|");
+    }
+}
