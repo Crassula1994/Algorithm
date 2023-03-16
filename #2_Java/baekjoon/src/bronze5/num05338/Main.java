3ac25ff56705ea3b907cad04668226cb0298dc4e
Crassula <gogun3535@gmail.com>
2023-03-16 21:32:05 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num05338/Main.java
@@ -0,0 +1,16 @@
+package bronze5.num05338;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) {
+
+		// 마이크로소프트 로고를 출력
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
