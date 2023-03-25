eff79acaf8dc6fc1de8e8a95329b7112b513518d
Crassula <gogun3535@gmail.com>
2023-03-25 16:18:11 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold3/num16236/Main.java
+++ #2_Java/baekjoon/src/gold3/num16236/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -56,6 +57,7 @@ public class Main {
 				
 				// 해당 값이 아기 상어의 위치인 경우 이를 배열 startLoc에 저장
 				if (map[r][c] == 9) {
+					map[r][c] = 0;
 					startLoc[0] = r;
 					startLoc[1] = c;
 				}
@@ -78,15 +80,26 @@ public class Main {
 	// fishCatcher() 메서드 정의 
 	public static void fishCatcher(int[] startLoc, int fishEaten, int sharkSize) {
 		
+		// 먹은 생선의 수가 상어의 크기와 같다면 먹은 생선의 수 및 상어의 크기 갱신
+		if (fishEaten == sharkSize) {
+			fishEaten = 0;
+			sharkSize++;
+		}
+		
 		// 해당 위치의 방문 여부를 저장할 2차원 배열 visited 초기화
-		boolean[][] visited = new boolean[size][size];
+		Integer[][] visited = new Integer[size][size];
 		
 		// 다음에 방문할 위치를 저장할 Queue 객체 visitList 초기화
 		Queue<int[]> visitList = new LinkedList<>();
 		
+		// 먹을 수 있는 물고기를 저장할 ProrityQueue 객체 edibleFishes 초기화
+		PriorityQueue<int[]> edibleFishes = new PriorityQueue<>((f1, f2) -> {
+			return (f1[0] == f2[0]) ? (f1[1] == f2[1]) ? f1[2] - f2[2] : f1[1] - f2[1] : f1[0] - f2[0];
+		});
+		
 		// offer() 메서드를 사용해 입력 받은 시작 위치를 visitList에 추가 및 방문 처리
 		visitList.offer(startLoc);
-		visited[startLoc[0]][startLoc[1]] = true;
+		visited[startLoc[0]][startLoc[1]] = 0;
 		
 		// while 반복문을 사용해 visitList가 빌 때까지 순회
 		while (!visitList.isEmpty()) {
@@ -94,47 +107,53 @@ public class Main {
 			// poll() 메서드를 사용해 계산할 값을 변수 currentLoc에 할당
 			int[] currentLoc = visitList.poll();
 			
-			// 해당 값이 최종 결과와 동일한 경우 반복문 탈출
-			if (currentVal == finalVal)
-				break;
+			// 해당 위치가 먹을 수 있는 물고기인 경우 
+			if (map[currentLoc[0]][currentLoc[1]] != 0 && map[currentLoc[0]][currentLoc[1]] < sharkSize) {
+				
+				// 해당 물고기까지의 거리, 위치를 배열 edibleFish에 저장
+				int[] edibleFish = {visited[currentLoc[0]][currentLoc[1]], currentLoc[0], currentLoc[1]};
+				
+				// offer() 메서드를 사용해 배열 edibleFish를 edibleFishes에 추가
+				edibleFishes.offer(edibleFish);
+			}
 
-			// 각 명령어를 수행한 결과를 각 변수에 할당
-			int resultD = (2 * currentVal) % 10000;
-			int resultS = (currentVal == 0) ? 9999 : currentVal - 1;
-			int resultL = (currentVal % 1000) * 10 + currentVal / 1000;
-			int resultR = (currentVal / 10) + (currentVal % 10) * 1000;
-			
-			// 'D' 명령어를 수행한 결과를 계산한 적이 없는 경우
-			if (calculated[resultD] == null) {
+			// for 반복문을 사용해 각 델타 배열을 순회
+			for (int d = 0; d < 4; d++) {
 				
-				// offer() 메서드를 사용해 계산 결과를 calculationList에 추가 및 방문 처리
-				calculationList.offer(resultD);
-				calculated[resultD] = calculated[currentVal] + "D";
-			}
-			
-			// 'S' 명령어를 수행한 결과를 계산한 적이 없는 경우
-			if (calculated[resultS] == null) {
+				// 새로운 위치를 각 변수에 할당
+				int nr = currentLoc[0] + dr[d];
+				int nc = currentLoc[1] + dc[d];
 				
-				// offer() 메서드를 사용해 계산 결과를 calculationList에 추가 및 방문 처리
-				calculationList.offer(resultS);
-				calculated[resultS] = calculated[currentVal] + "S";
+				// 해당 위치가 공간을 벗어나지 않고, 방문하지 않은 위치이며 상어 크기와 같거나 작은 경우
+				if (nr >= 0 && nr < size && nc >=0 && nc < size && visited[nr][nc] == null && map[nr][nc] <= sharkSize) {
+					
+					// 해당 위치를 배열 nextLoc에 저장
+					int[] nextLoc = {nr, nc};
+					
+					// offer() 메서드를 사용해 해당 위치를 visitList에 추가 및 방문 처리
+					visitList.offer(nextLoc);
+					visited[nr][nc] = visited[currentLoc[0]][currentLoc[1]] + 1;
+				}
 			}
+		}
+		
+		// 먹을 수 있는 물고기가 한 마리 이상 있는 경우
+		if (!edibleFishes.isEmpty()) {
 			
-			// 'L' 명령어를 수행한 결과를 계산한 적이 없는 경우
-			if (calculated[resultL] == null) {
-				
-				// offer() 메서드를 사용해 계산 결과를 calculationList에 추가 및 방문 처리
-				calculationList.offer(resultL);
-				calculated[resultL] = calculated[currentVal] + "L";
-			}
+			// poll() 메서드를 사용해 가장 우선순위가 높은 물고기를 배열 selectedFish에 할당
+			int[] selectedFish = edibleFishes.poll();
+
+			// 해당 위치로 이동하기까지의 시간을 갱신
+			maxTime += selectedFish[0];
 			
-			// 'R' 명령어를 수행한 결과를 계산한 적이 없는 경우
-			if (calculated[resultR] == null) {
-				
-				// offer() 메서드를 사용해 계산 결과를 calculationList에 추가 및 방문 처리
-				calculationList.offer(resultR);
-				calculated[resultR] = calculated[currentVal] + "R";
-			}
+			// 해당 위치의 물고기를 제거
+			map[selectedFish[1]][selectedFish[2]] = 0;
+			
+			// 아기 상어의 위치를 배열 nextLoc에 저장
+			int[] nextLoc = {selectedFish[1], selectedFish[2]};
+			
+			// fishCatcher() 메서드 재귀 호출
+			fishCatcher(nextLoc, fishEaten + 1, sharkSize);
 		}
 	}
 }
