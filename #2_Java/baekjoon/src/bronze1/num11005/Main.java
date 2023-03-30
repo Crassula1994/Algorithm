b078d62d39e36bbff3a703c7294f3f10acbfc336
gogun3535 <gogun3535@gmail.com>
2023-03-30 14:56:57 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze1/num11005/Main.java
@@ -0,0 +1,38 @@
+package bronze1.num11005;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 숫자 및 진법을 각 변수에 할당
+		int number = Integer.parseInt(st.nextToken());
+		int radix = Integer.parseInt(st.nextToken());
+		
+		// toString() 및 toUpperCase() 메서드를 사용해 입력 받은 숫자를 해당 진법으로 바꾼 숫자를 변수 answer에 할당
+		String answer = Integer.toString(number, radix).toUpperCase();
+		
+		// write() 메서드를 사용해 해당 진법으로 바꾼 숫자를 출력
+		out.write(answer);
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
