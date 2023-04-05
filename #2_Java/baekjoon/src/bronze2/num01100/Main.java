e2e9a5412af187e0b5541bb967e2da5e9fb55b29
Crassula <gogun3535@gmail.com>
2023-04-05 23:27:43 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze2/num01100/Main.java
@@ -0,0 +1,51 @@
+package bronze2.num01100;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 체스판의 상태를 저장할 2차원 배열 chessBoard 초기화
+		char[][] chessBoard = new char[8][8];
+		
+		// 하얀 칸에 위치한 말의 개수를 저장할 변수 pieceNum 초기화
+		int pieceNum = 0;
+		
+		// for 반복문을 사용해 배열 chessBoard의 각 행을 순회
+		for (int r = 0; r < 8; r++) {
+			
+			// readLine() 메서드를 사용해 입력 받은 체스판 행의 상태를 변수 line에 할당
+			String line = in.readLine();
+			
+			// for 반복문을 사용해 배열 chessBoard의 각 열을 순회
+			for (int c = 0; c < 8; c++) {
+				
+				// charAt() 메서드를 사용해 배열 chessBoard에 체스판의 상태를 저장
+				chessBoard[r][c] = line.charAt(c);
+				
+				// 해당 위치가 하얀 칸이고 말이 존재하는 경우 말의 개수를 갱신
+				if (r % 2 == c % 2 && chessBoard[r][c] == 'F')
+					pieceNum++;
+			}
+		}
+		
+		// valueOf() 및 write() 메서드를 사용해 하얀 칸에 위치한 말의 개수를 출력
+		out.write(String.valueOf(pieceNum)); 
+
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
