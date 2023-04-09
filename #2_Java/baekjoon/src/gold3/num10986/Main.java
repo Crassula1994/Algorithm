e8ccbe1815297cd894a27acc2c8b1c18833d7c8f
Crassula <gogun3535@gmail.com>
2023-04-09 11:35:22 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold3/num10986/Main.java
+++ #2_Java/baekjoon/src/gold3/num10986/Main.java
@@ -11,16 +11,6 @@ import java.util.StringTokenizer;
 // Main 클래스 정의
 public class Main {
 	
-	// 수열의 길이, 나누는 숫자, 나누는 숫자로 나누어 떨어지는 구간의 개수를 나타낼 각 변수 초기화
-	static int length;
-	static int divisor;
-	static int count;
-	
-	// 각 숫자까지의 누적 합을 저장할 배열 sequenceSum 초기화
-	static long[] sequenceSum;
-	
-	// ----------------------------------------------------------------------------------------------------
-	
 	// main() 메서드 정의
 	public static void main(String[] args) throws IOException {
 
@@ -31,40 +21,50 @@ public class Main {
 		// StringTokenizer 객체를 불러와 변수 st에 할당
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		
-		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 수열의 길이, 나누는 숫자를 각 변수에 할당
-		length = Integer.parseInt(st.nextToken());
-		divisor = Integer.parseInt(st.nextToken());
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 수열의 길이, 제수를 각 변수에 할당
+		int length = Integer.parseInt(st.nextToken());
+		int divisor = Integer.parseInt(st.nextToken());
 		
-		// 각 숫자까지의 누적 합을 저장할 배열 sequenceSum 초기화
-		sequenceSum = new long[length + 1];
+		// 각 숫자까지의 누적 합을 제수로 나눈 나머지와 해당 나머지의 개수를 저장할 각 배열 초기화
+		long[] sumRemain = new long[length + 1];
+		long[] remainCnt = new long[divisor];
+		
+		// 제수로 나누어 떨어지는 구간의 개수를 저장할 변수 totalCnt 초기화
+		long totalCnt = 0;
 		
 		// StringTokenizer 객체를 불러와 변수 st에 재할당
 		st = new StringTokenizer(in.readLine());
 		
-		// for 반복문을 사용해 수열의 각 숫자를 순회하며 누적 합을 갱신
-		for (int idx = 1; idx <= length; idx++)
-			sequenceSum[idx] = sequenceSum[idx - 1] + (Integer.parseInt(st.nextToken()) % divisor);
+		// for 반복문을 사용해 수열의 각 숫자를 순회
+		for (int idx = 1; idx <= length; idx++) {
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 배열 sumRemain에 누적 합을 제수로 나눈 나머지를 저장
+			sumRemain[idx] = (sumRemain[idx - 1] + Integer.parseInt(st.nextToken())) % divisor;
+			
+			// 제수로 나눈 나머지가 0인 경우 제수로 나누어 떨어지는 구간의 개수를 갱신
+			if (sumRemain[idx] == 0)
+				totalCnt++;
+			
+			// 해당 나머지의 개수를 갱신
+			remainCnt[(int) sumRemain[idx]]++;
+		}
+
+		// for 반복문을 사용해 배열 remainCnt의 각 원소를 순회
+		for (int idx = 0; idx < remainCnt.length; idx++) {
+			
+			// 해당 구간의 개수가 1개 이하인 경우 다음 원소를 순회
+			if (remainCnt[idx] < 2)
+				continue;
+			
+			// 제수로 나누어 떨어지는 구간의 개수를 갱신
+			totalCnt += (remainCnt[idx] * (remainCnt[idx] - 1)) / 2;
+		}
 		
-		// 나누는 숫자로 나누어 떨어지는 구간의 개수를 저장할 변수 count 초기화
-		count = 0;
-		
-		// divisibleCounter() 메서드를 호출해 나누는 숫자로 나누어 떨어지는 구간의 개수를 갱신
-		divisibleCounter(1);
-		
-		// valueOf() 및 write() 메서드를 사용해 나누는 숫자로 나누어 떨어지는 구간의 개수를 출력
-		out.write(String.valueOf(count));
+		// valueOf() 및 write() 메서드를 사용해 제수로 나누어 떨어지는 구간의 개수를 출력
+		out.write(String.valueOf(totalCnt));
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
 		out.close();
-	}
-
-	// ----------------------------------------------------------------------------------------------------
-	
-	// divisibleCounter() 메서드 정의
-	public static void divisibleCounter(int startIdx) {
-		
-		// 
-		
 	}
 }
