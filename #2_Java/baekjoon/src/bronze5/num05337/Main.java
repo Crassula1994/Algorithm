270bb145b6c6b7836cae429ad9fb2a14d07ffae9
Crassula1994 <gogun3535@gmail.com>
2023-04-14 19:54:45 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num05337/Main.java
@@ -0,0 +1,14 @@
+package bronze5.num05337;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) {
+
+		// 'Welcome'을 예제와 같이 출력
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
