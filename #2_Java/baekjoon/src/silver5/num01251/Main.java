30675b5b74dc7e8396ab998197415b2402234e09
Crassula1994 <gogun3535@gmail.com>
2023-05-06 03:22:26 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/silver5/num01251/Main.java
+++ #2_Java/baekjoon/src/silver5/num01251/Main.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
 
 // Main 클래스 정의
 public class Main {
@@ -20,20 +21,25 @@ public class Main {
 		// readLine() 메서드를 사용해 입력 받은 단어를 변수 word에 할당
 		String word = in.readLine();
 		
-		// for 반복문을 사용해 
+		// 각 단어를 나누고 뒤집은 결과를 저장할 PriorityQueue 객체 wordList 초기화
+		PriorityQueue<String> wordList = new PriorityQueue<>();
 		
-		// for 반복문을 사용해 각 단어를 순회
-		if (month == 2 && day == 18) {
-			out.write("Special");
-		
-		// 해당 날짜가 2월 18일 이전인 경우 'Before' 출력
-		} else if (month < 2 || (month == 2 && day < 18)) {
-			out.write("Before");
-		
-		// 해당 날짜가 2월 18일 이후인 경우 'After' 출력
-		} else {
-			out.write("After");
+		// for 반복문을 사용해 각각 단어를 나눌 위치를 순회
+		for (int i = 1; i < word.length() - 1; i++) {
+			for (int j = i + 1; j < word.length(); j++) {
+				
+				// substring(), reverse(), toString() 메서드를 사용해 해당 단어를 세 부분으로 나누고 뒤집은 결과를 각 변수에 저장
+				String firstWord = new StringBuilder(word.substring(0, i)).reverse().toString();
+				String secondWord = new StringBuilder(word.substring(i, j)).reverse().toString();
+				String thirdWord = new StringBuilder(word.substring(j, word.length())).reverse().toString();
+				
+				// offer() 메서드를 사용해 각 단어를 결합한 결과를 wordList에 저장
+				wordList.offer(firstWord + secondWord + thirdWord);
+			}
 		}
+		
+		// poll() 및 write() 메서드를 사용해 사전순으로 가장 앞서는 단어를 출력
+		out.write(wordList.poll());
 
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
