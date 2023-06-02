39571b69781b493935743874057bf2904b7650bf
Crassula1994 <gogun3535@gmail.com>
2023-06-02 23:15:38 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num02441/Main.java
@@ -0,0 +1,42 @@
+package bronze3.num02441;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 줄의 갯수를 변수 lineNum에 할당
+		int lineNum = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 줄을 순회
+		for (int line = 0; line < lineNum; line++) {
+			
+			// for 반복문을 사용해 빈 칸을 출력
+			for (int b = 0; b < line; b++)
+				out.write(" ");
+			
+			// for 반복문을 사용해 별을 출력
+			for (int s = 0; s < lineNum - line; s++)
+				out.write("*");
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
