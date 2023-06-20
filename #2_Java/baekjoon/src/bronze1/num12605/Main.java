40ecb1a6ab64d8360b953c551d410a6822de6240
Crassula1994 <gogun3535@gmail.com>
2023-06-20 15:55:17 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze1/num12605/Main.java
@@ -0,0 +1,53 @@
+package bronze1.num12605;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 테스트 케이스를 순회
+		for (int tc = 1; tc <= testCase; tc++) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// 각 단어를 저장할 Stack 객체 words 초기화
+			Stack<String> words = new Stack<>();
+			
+			// while 반복문을 사용해 각 단어를 words에 추가
+			while (st.hasMoreTokens())
+				words.push(st.nextToken());
+			
+			// write() 메서드를 사용해 케이스 번호를 출력
+			out.write("Case #" + tc + ": ");
+			
+			// while 반복문을 사용해 words가 빌 때까지 각 단어를 거꾸로 출력
+			while (!words.isEmpty())
+				out.write(words.pop() + " ");
+			
+			// newLine() 메서드를 사용해 줄바꿈 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
