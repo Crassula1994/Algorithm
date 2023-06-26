9a4d83743bbc5d59fb60dd311d5f41dccfcbfad0
Crassula1994 <gogun3535@gmail.com>
2023-06-26 23:59:04 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num13866/Main.java
@@ -0,0 +1,45 @@
+package bronze4.num13866;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// 
+		int[] skillLevels = new int[4];
+		
+		// for 반복문을 사용해 각 스킬 레벨을 배열 skillLevels에 저장
+		for (int idx = 0; idx < 4; idx++)
+			skillLevels[idx] = Integer.parseInt(st.nextToken());
+
+		
+		// 
+		int minDifference = Integer.MAX_VALUE;
+		
+		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[1] - skillLevels[2] - skillLevels[3]), minDifference);
+		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[2] - skillLevels[1] - skillLevels[3]), minDifference);
+		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[3] - skillLevels[1] - skillLevels[2]), minDifference);
+	
+		out.write(String.valueOf(minDifference));
+
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
