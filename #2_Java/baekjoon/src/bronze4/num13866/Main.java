054d80ec7eb6c3a57fb3d8eee0c29c25850a085a
Crassula1994 <gogun3535@gmail.com>
2023-06-27 00:08:17 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/bronze4/num13866/Main.java
+++ #2_Java/baekjoon/src/bronze4/num13866/Main.java
@@ -21,21 +21,35 @@ public class Main {
 		// StringTokenizer 객체를 불러와 변수 st에 할당
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		
-		// 
+		// 네 명의 스킬 레벨을 저장할 배열 skillLevels에 저장
 		int[] skillLevels = new int[4];
 		
-		// for 반복문을 사용해 각 스킬 레벨을 배열 skillLevels에 저장
+		// for 반복문을 사용해 입력 받은 각 스킬 레벨을 배열 skillLevels에 저장
 		for (int idx = 0; idx < 4; idx++)
 			skillLevels[idx] = Integer.parseInt(st.nextToken());
 
-		
-		// 
+		// 두 팀의 스킬 레벨 차이의 최솟값을 출력할 변수 minDiffernce 초기화
 		int minDifference = Integer.MAX_VALUE;
 		
-		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[1] - skillLevels[2] - skillLevels[3]), minDifference);
-		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[2] - skillLevels[1] - skillLevels[3]), minDifference);
-		minDifference = Math.min(Math.abs(skillLevels[0] + skillLevels[3] - skillLevels[1] - skillLevels[2]), minDifference);
-	
+		// for 반복문을 사용해 가능한 두 팀의 조합을 순회
+		for (int c = 1; c < 4; c++) {
+			
+			// 각 팀의 스킬 레벨의 합을 각 변수에 저장
+			int team1 = skillLevels[0] + skillLevels[c];
+			int team2 = skillLevels[1] + skillLevels[2] + skillLevels[3] - skillLevels[c];
+			
+			// abs() 메서드를 사용해 두 팀의 스킬 레벨의 차이를 변수 difference에 할당
+			int difference = Math.abs(team1 - team2);
+			
+			// min() 메서드를 사용해 두 팀의 스킬 레벨 차이의 최솟값을 갱신
+			minDifference = Math.min(difference, minDifference);
+			
+			// 두 팀의 스킬 레벨의 차이가 없는 경우 반복문 탈출
+			if (minDifference == 0)
+				break;
+		}
+		
+		// valueOf() 및 write() 메서드를 사용해 두 팀의 스킬 레벨 차이의 최솟값을 출력
 		out.write(String.valueOf(minDifference));
 
 		// close() 메서드를 사용해 각 객체 종료
