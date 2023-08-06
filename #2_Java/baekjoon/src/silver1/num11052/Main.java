1659fe606c0fbb26843004c7e9fc9653a42b7232
Crassula1994 <gogun3535@gmail.com>
2023-08-07 00:29:39 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/silver1/num11052/Main.java
@@ -0,0 +1,48 @@
+package silver1.num11052;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 구매하려는 카드의 개수를 변수 cardNum에 할당
+		int cardNum = Integer.parseInt(in.readLine());
+		
+		// 각 카드팩의 가격, 각 카드의 개수마다 지불해야 하는 금액의 최댓값을 저장할 각 배열 초기화
+		int[] prices = new int[cardNum + 1];
+		int[] maxPrices = new int[cardNum + 1];
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// for 반복문을 사용해 입력 받은 각 카드팩의 가격을 배열 price에 저장
+		for (int idx = 1; idx <= cardNum; idx++)
+			prices[idx] = Integer.parseInt(st.nextToken());
+		
+		// for 반복문을 사용해 각 카드의 개수마다 지불해야 하는 금액의 최댓값을 갱신
+		for (int i = 1; i <= cardNum; i++) {
+			for (int j = 1; j <= i; j++)
+				maxPrices[i] = Math.max(maxPrices[i], maxPrices[i - j] + prices[j]);
+		}
+		
+		// valueOf() 및 write() 메서드를 사용해 지불해야 하는 금액의 최댓값을 출력
+		out.write(String.valueOf(maxPrices[cardNum]));
+	
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
