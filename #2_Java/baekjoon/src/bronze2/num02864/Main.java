66eeb0042860540f733f94f7ae8ff15c914a35eb
Crassula1994 <gogun3535@gmail.com>
2023-11-26 14:46:41 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/bronze2/num02864/Main.java
+++ #2_Java/baekjoon/src/bronze2/num02864/Main.java
@@ -25,55 +25,49 @@ public class Main {
 		String numberA = st.nextToken();
 		String numberB = st.nextToken();
 		
-		// 두 숫자의 최솟값과 최댓값을 저장할 각 변수 초기화
-		int minA = 0;
-		int minB = 0;
-		int maxA = 0;
-		int maxB = 0;
+		// extremeValueMaker() 메서드를 호출해 두 숫자의 최솟값과 최댓값을 각 배열에 할당
+		int[] extremeA = extremeValueMaker(numberA);
+		int[] extremeB = extremeValueMaker(numberB);
 		
-		// for 반복문을 사용해 정수 A의 각 자릿수를 순회
-		for (int idx = 0; idx < numberA.length(); idx++) {
-			
-			// charAt() 메서드를 사용해 각 자릿수를 변수 cipher에 할당
-			int cipher = numberA.charAt(idx) - '0';
-			
-			// 해당 숫자가 5나 6인 경우 정수 A의 최솟값과 최댓값을 갱신
-			if (cipher == 5 || cipher == 6) {
-				minA = minA * 10 + 5;
-				maxA = maxA * 10 + 6;
-				
-			// 해당 숫자가 5나 6이 아닌 숫자인 경우 정수 A의 최솟값과 최댓값을 갱신
-			} else {
-				minA = minA * 10 + cipher;
-				maxA = maxA * 10 + cipher;
-			}
-		}
+		// 두 수의 합 중 최솟값과 최댓값을 계산해 각 변수에 할당
+		int minSum = extremeA[0] + extremeB[0];
+		int maxSum = extremeA[1] + extremeB[1];
 		
-		// for 반복문을 사용해 정수 B의 각 자릿수를 순회
-		for (int idx = 0; idx < numberB.length(); idx++) {
-			
-			// charAt() 메서드를 사용해 각 자릿수를 변수 cipher에 할당
-			int cipher = numberB.charAt(idx) - '0';
-			
-			// 해당 숫자가 5나 6인 경우 정수 B의 최솟값과 최댓값을 갱신
-			if (cipher == 5 || cipher == 6) {
-				minB = minB * 10 + 5;
-				maxB = maxB * 10 + 6;
-				
-			// 해당 숫자가 5나 6이 아닌 숫자인 경우 정수 B의 최솟값과 최댓값을 갱신
-			} else {
-				minB = minB * 10 + cipher;
-				maxB = maxB * 10 + cipher;
-			}
-		}
-		
-		int minSum = minA + minB;
-		int maxSum = maxA + maxB;
-		
+		// write() 메서드를 사용해 두 수의 합 중 최솟값과 최댓값을 출력
 		out.write(minSum + " " + maxSum);
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
 		out.close();
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+	
+	// extremeValueMaker() 메서드 정의
+	private static int[] extremeValueMaker(String numberStr) {
+		
+		// 최솟값과 최댓값을 저장할 배열 extremeVal 초기화
+		int[] extremeVal = new int[2];
+		
+		// for 반복문을 사용해 정수의 각 자릿수를 순회
+		for (int idx = 0; idx < numberStr.length(); idx++) {
+			
+			// charAt() 메서드를 사용해 각 자릿수를 변수 cipher에 할당
+			int cipher = numberStr.charAt(idx) - '0';
+			
+			// 해당 숫자가 5나 6인 경우 정수의 최솟값과 최댓값을 갱신
+			if (cipher == 5 || cipher == 6) {
+				extremeVal[0] = extremeVal[0] * 10 + 5;
+				extremeVal[1] = extremeVal[1] * 10 + 6;
+				
+			// 해당 숫자가 5나 6이 아닌 숫자인 경우 정수 A의 최솟값과 최댓값을 갱신
+			} else {
+				extremeVal[0] = extremeVal[0] * 10 + cipher;
+				extremeVal[1] = extremeVal[1] * 10 + cipher;
+			}
+		}
+		
+		// 최솟값과 최댓값을 저장한 배열 반환
+		return extremeVal;
 	}
 }
