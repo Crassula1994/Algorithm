f38f0a084c4579d39fcfd55a30f046fd17e3fb57
Crassula <gogun3535@gmail.com>
2024-01-14 22:45:26 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num25628/Main.java
@@ -0,0 +1,38 @@
+package bronze4.num25628;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 빵의 개수와 햄버거 패티의 개수를 각 변수에 할당
+		int bunNum = Integer.parseInt(st.nextToken());
+		int pattyNum = Integer.parseInt(st.nextToken());
+		
+		// min() 메서드를 사용해 만들 수 있는 햄버거의 최대 개수를 계산하고 변수 maxCount에 할당
+		int maxCount = Math.min(pattyNum, bunNum / 2);
+		
+		// valueOf() 및 write() 메서드를 사용해 만들 수 있는 햄버거의 최대 개수를 출력
+		out.write(String.valueOf(maxCount));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
