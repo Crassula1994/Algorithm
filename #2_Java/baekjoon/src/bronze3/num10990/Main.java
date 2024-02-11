e0d403e4d3723f50580c0ba4fc4e9d3c2dbdeffc
Crassula <gogun3535@gmail.com>
2024-02-11 12:41:10 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num10990/Main.java
@@ -0,0 +1,47 @@
+package bronze3.num10990;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 숫자를 변수 number에 할당
+		int number = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 행을 순회
+		for (int r = 0; r < number; r++) {
+			
+			// for 반복문을 사용해 각 열을 순회
+			for (int c = 0; c < number + r; c++) {
+				
+				// 별을 출력해야 하는 경우 별을 출력
+				if (c == number - r - 1 || c == number + r - 1) {
+					out.write("*");
+					
+				// 빈 칸을 출력해야 하는 경우 빈 칸을 출력
+				} else {
+					out.write(" ");
+				}
+			}
+			
+			// newLine() 메서드를 사용해 줄바꿈 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
