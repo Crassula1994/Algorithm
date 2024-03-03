d3bed177406560136318be3bf7d762ac36bdec0c
Crassula <gogun3535@gmail.com>
2024-03-03 23:44:15 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/bronze2/num01408/Main.java
+++ #2_Java/baekjoon/src/bronze2/num01408/Main.java
@@ -32,17 +32,8 @@ public class Main {
 		int startTime = Integer.parseInt(st.nextToken()) * 3600 + Integer.parseInt(st.nextToken()) * 60
 				+ Integer.parseInt(st.nextToken());
 		
-		// 임무를 수행하는 데 남은 시간을 저장할 변수 remainTime 초기화
-		int remainTime = 0;
-		
-		// 현재 시간이 임무 시작 시간 이전인 경우 임무를 수행하는 데 남은 시간 갱신
-		if (curTime < startTime) {
-			remainTime = startTime - curTime;
-		
-		// 현재 시간이 임무 시작 시간 이후인 경우 임무를 수행하는 데 남은 시간 갱신
-		} else {
-			remainTime = startTime - curTime + 86400;
-		}
+		// 임무를 수행하는 데 남은 시간을 계산해 변수 remainTime에 할당
+		int remainTime = (curTime < startTime) ? startTime - curTime : startTime - curTime + 86400;
 		
 		// format() 및 write() 메서드를 사용해 도현이가 임무를 수행하는 데 남은 시간을 출력
 		out.write(String.format("%02d:%02d:%02d", remainTime / 3600, (remainTime / 60) % 60, remainTime % 60));
