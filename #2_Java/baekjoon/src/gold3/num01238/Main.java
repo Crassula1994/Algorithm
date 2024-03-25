53480076eb0b64839ea2b18981f728462e6e1035
Crassula <gogun3535@gmail.com>
2024-03-26 00:05:10 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold3/num01238/Main.java
+++ #2_Java/baekjoon/src/gold3/num01238/Main.java
@@ -9,6 +9,7 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 // Main 클래스 정의
@@ -18,10 +19,6 @@ public class Main {
 	static int studentNum;
 	static int roadNum;
 	static int targetTown;
-	
-	// 연결된 도로의 정보를 저장할 각 배열 초기화
-	static List<int[]>[] toRoads;
-	static List<int[]>[] fromRoads;
 	
 	// ----------------------------------------------------------------------------------------------------
 	
@@ -42,22 +39,14 @@ public class Main {
 		targetTown = Integer.parseInt(st.nextToken());
 		
 		// 연결된 도로의 정보를 저장할 각 배열 초기화
-		toRoads = new ArrayList[studentNum + 1];
-		fromRoads = new ArrayList[studentNum + 1];	
+		List<int[]>[] toRoads = new ArrayList[studentNum + 1];
+		List<int[]>[] fromRoads = new ArrayList[studentNum + 1];	
 		
 		// for 반복문을 사용해 연결된 도로의 정보를 저장할 각 배열의 원소를 초기화
 		for (int idx = 0; idx <= studentNum; idx++) {
 			toRoads[idx] = new ArrayList<>();
 			fromRoads[idx] = new ArrayList<>();			
 		}
-		
-		// 해당 마을과 목적지 마을 사이의 최소 소요 시간의 값을 저장할 각 배열 초기화
-		int[] toMinTime = new int[studentNum + 1];
-		int[] fromMinTime = new int[studentNum + 1];
-		
-		// fill() 메서드를 사용해 각 배열의 최소 소요 시간의 값을 초기화
-		Arrays.fill(toMinTime, Integer.MAX_VALUE);
-		Arrays.fill(fromMinTime, Integer.MAX_VALUE);
 		
 		// for 반복문을 사용해 각 도로의 정보를 순회
 		for (int road = 0; road < roadNum; road++) {
@@ -75,9 +64,19 @@ public class Main {
 			fromRoads[fromTown].add(new int[] {toTown, time});
 		}
 		
-		// timeCalculator() 메서드를 호출해 최소 소요 시간의 값을 갱신
-		timeCalculator(toMinTime);
-		timeCalculator(fromMinTime);
+		// timeCalculator() 메서드를 호출해 최소 소요 시간의 배열을 각 배열에 할당
+		int[] toMinTime = timeCalculator(toRoads);
+		int[] fromMinTime = timeCalculator(fromRoads);
+		
+		// 가장 오래 걸리는 학생의 소요 시간을 저장할 변수 maxTime 초기화
+		int maxTime = 0;
+		
+		// for 반복문을 사용해 가장 오래 걸리는 학생의 소요 시간을 갱신
+		for (int student = 1; student <= studentNum; student++)
+			maxTime = Math.max(toMinTime[student] + fromMinTime[student], maxTime);
+		
+		// valueOf() 및 write() 메서드를 사용해 가장 오래 걸리는 학생의 소요 시간을 출력
+		out.write(String.valueOf(maxTime));
 
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
@@ -86,8 +85,56 @@ public class Main {
 	
 	// ----------------------------------------------------------------------------------------------------
 	
-	public static void timeCalculator(int[] fromMinTime) {
-		// TODO Auto-generated method stub
+	// timeCalculator() 메서드 정의
+	public static int[] timeCalculator(List<int[]>[] roads) {
 		
+		// 다음에 이동할 마을을 저장할 PriorityQueue 객체 moveList 초기화
+		PriorityQueue<int[]> moveList = new PriorityQueue<>((t1, t2) -> {
+			return t1[1] - t2[1];
+		});
+		
+		// 목적지 마을과 다른 마을 사이의 최소 소요 시간, 해당 마을의 이동 여부를 저장할 각 배열 초기화
+		int[] minTime = new int[studentNum + 1];
+		boolean[] moved = new boolean[studentNum + 1];
+		
+		// fill() 메서드를 사용해 목적지 마을과 다른 마을 사이의 최소 소요 시간의 값을 초기화
+		Arrays.fill(minTime, Integer.MAX_VALUE);
+		
+		// 목적지 마을의 최소 소요 시간을 설정
+		minTime[targetTown] = 0;
+		
+		// offer() 메서드를 사용해 목적지 마을을 moveList에 추가
+		moveList.offer(new int[] {targetTown, 0});
+		
+		// while 반복문을 사용해 moveList가 빌 때까지 순회
+		while (!moveList.isEmpty()) {
+			
+			// poll() 메서드를 사용해 현재 이동 중인 마을을 변수 currentTown에 할당
+			int[] currentTown = moveList.poll();
+			
+			// 해당 마을을 이동한 적이 없는 경우
+			if (!moved[currentTown[0]]) {
+				
+				// 현재 이동 중인 마을을 이동 처리
+				moved[currentTown[0]] = true;
+				
+				// for 반복문을 사용해 해당 마을과 연결된 다른 마을을 순회
+				for (int idx = 0; idx < roads[currentTown[0]].size(); idx++) {
+					
+					// get() 메서드를 사용해 연결된 마을 및 소요 시간을 각 변수에 할당
+					int town = roads[currentTown[0]].get(idx)[0];
+					int time = roads[currentTown[0]].get(idx)[1];
+					
+					// 해당 마을까지의 최소 소요 시간이 저장된 최소 소요 시간보다 적은 경우 최소 소요 시간 갱신 후 moveList에 추가
+					if (minTime[town] > minTime[currentTown[0]] + time) {
+						minTime[town] = minTime[currentTown[0]] + time;
+						moveList.offer(new int[] {town, minTime[town]});
+					}
+				}
+			}
+		}
+		
+		// 목적지 마을과 다른 마을 사이의 최소 소요 시간을 저장한 배열 반환
+		return minTime;
 	}
 }
