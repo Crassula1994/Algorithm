f2b652950095832f037acc0178d6780dc466698c
Crassula <gogun3535@gmail.com>
2024-06-11 15:52:40 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/silver5/num01402/Main.java
@@ -0,0 +1,46 @@
+package silver5.num01402;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 개수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 테스트 케이스를 순회
+		for (int tc = 0; tc < testCase; tc++) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 두 정수를 각 변수에 할당
+			int numberA = Integer.parseInt(st.nextToken());
+			int numberB = Integer.parseInt(st.nextToken());
+			
+			// write() 메서드를 사용해 모든 정수는 변환 가능하므로 'yes'를 출력
+			out.write("yes");
+			
+			// newLine() 메서드를 사용해 줄바꿈 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
