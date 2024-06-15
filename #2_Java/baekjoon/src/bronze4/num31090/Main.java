d77d04379a7d2bd0633148b28b23bfcde8a368ac
Crassula <gogun3535@gmail.com>
2024-06-15 14:05:50 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num31090/Main.java
@@ -0,0 +1,40 @@
+package bronze4.num31090;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 개수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 테스트 케이스를 순회
+		for (int tc = 0; tc < testCase; tc++) {
+			
+			// readLine() 및 parseInt() 메서드를 사용해 입력 받은 연도를 변수 year에 할당
+			int year = Integer.parseInt(in.readLine());
+			
+			// write() 메서드를 사용해 다음 연도가 현재 연도의 끝 두 자리로 나누어 떨어지는지 여부에 따라 정해진 문구를 출력
+			out.write(((year + 1) % (year % 100) == 0) ? "Good" : "Bye");
+					
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
