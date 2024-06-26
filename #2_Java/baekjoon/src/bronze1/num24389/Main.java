a82e6bf96f34829b00928f78eedf2f976820cc65
Crassula <gogun3535@gmail.com>
2024-06-26 15:40:58 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze1/num24389/Main.java
@@ -0,0 +1,33 @@
+package bronze1.num24389;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 정수를 변수 number에 할당
+		int number = Integer.parseInt(in.readLine());
+		
+		// bitCount() 메서드를 사용해 주어진 정수와 그 보수의 서로 다른 비트 수를 변수 count에 할당
+		int count = Integer.bitCount(number ^ (~number + 1));
+		
+		// valueOf() 및 write() 메서드를 사용해 주어진 정수와 그 보수의 서로 다른 비트 수를 출력
+		out.write(String.valueOf(count));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
