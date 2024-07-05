87bb8c6084d5ec4223635d8beaa9d664bd489f55
Crassula <gogun3535@gmail.com>
2024-07-05 22:51:59 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num09295/Main.java
@@ -0,0 +1,41 @@
+package bronze4.num09295;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 테스트 케이스의 수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// for 반복문을 사용해 각 테스트 케이스를 순회
+		for (int tc = 1; tc <= testCase; tc++) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 두 주사위의 합을 변수 sum에 할당
+			int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+			
+			// write() 메서드를 사용해 주사위를 두 번 던져 나온 두 수의 합을 출력
+			out.write("Case " + tc + ": " + sum + "\n");
+		}
+			
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
