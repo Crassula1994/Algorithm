5cfd64fc17588cc2589e2965c14adeaf54ef1ce1
Crassula <gogun3535@gmail.com>
2024-07-15 10:37:04 +0900

    Programmers Java Solution Added

---
+++ #2_Java/programmers/src/level0/num120803/Solution.java
@@ -0,0 +1,12 @@
+package level0.num120803;
+
+// Solution 클래스 정의
+public class Solution {
+	
+	// solution() 메서드 정의
+	public static int solution(int num1, int num2) {
+		
+		// 두 숫자의 차이를 반환
+		return num1 - num2;
+    }
+}
