71fad8f4063ffa037f58d7efe9e741a68a630580
Crassula <gogun3535@gmail.com>
2024-07-15 10:40:50 +0900

    Programmers Java Solution Added

---
+++ #2_Java/programmers/src/level0/num120807/Solution.java
@@ -0,0 +1,12 @@
+package level0.num120807;
+
+// Solution 클래스 정의
+public class Solution {
+	
+	// solution() 메서드 정의
+	public static int solution(int num1, int num2) {
+		
+		// 두 숫자가 같은지 여부를 반환
+		return (num1 == num2) ? 1 : -1;
+	}
+}
