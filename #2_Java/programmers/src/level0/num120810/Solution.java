34d3e9b6087217a8f9c7392f13fad65eb6f63636
Crassula <gogun3535@gmail.com>
2024-07-15 10:56:17 +0900

    Programmers Java Solution Added

---
+++ #2_Java/programmers/src/level0/num120810/Solution.java
@@ -0,0 +1,12 @@
+package level0.num120810;
+
+// Solution 클래스 정의
+public class Solution {
+	
+	// solution() 메서드 정의
+	public static int solution(int num1, int num2) {
+		
+		// 첫 번째 수를 두 번째 수로 나눈 나머지를 반환
+		return num1 % num2;
+    }
+}
