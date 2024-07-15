afbd92022e5931ec04c52b8457eb2a741ed5cc82
Crassula <gogun3535@gmail.com>
2024-07-15 10:45:08 +0900

    Programmers Java Solution Added

---
+++ #2_Java/programmers/src/level0/num120802/Solution.java
@@ -0,0 +1,12 @@
+package level0.num120802;
+
+// Solution 클래스 정의
+public class Solution {
+	
+	// solution() 메서드 정의
+	public static int solution(int num1, int num2) {
+		
+		// 두 숫자의 합을 반환
+		return num1 + num2;
+    }
+}
