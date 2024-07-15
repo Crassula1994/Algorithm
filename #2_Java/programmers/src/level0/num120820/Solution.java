2119025237807f222f2320f3089244feff61d6e1
Crassula <gogun3535@gmail.com>
2024-07-15 10:43:01 +0900

    Programmers Java Solution Added

---
+++ #2_Java/programmers/src/level0/num120820/Solution.java
@@ -0,0 +1,12 @@
+package level0.num120820;
+
+// Solution 클래스 정의
+public class Solution {
+	
+	// solution() 메서드 정의
+	public static int solution(int age) {
+		
+		// 선생님의 출생 연도를 반환
+		return 2023 - age;
+	}
+}
