e86e5644566091d81328523adca54d1fab85eb61
Crassula <gogun3535@gmail.com>
2024-07-20 23:58:23 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num30214/Main.java
@@ -0,0 +1,35 @@
+package bronze5.num30214;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 절반 및 마지막에 문제를 푼 횟수를 각 변수에 할당
+		int middleCount = Integer.parseInt(st.nextToken());
+		int endCount = Integer.parseInt(st.nextToken());
+		
+		// write() 메서드를 사용해 해당 문제가 쉬운 문제인지 여부를 출력
+		out.write((middleCount * 2 < endCount) ? "H" : "E");
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
