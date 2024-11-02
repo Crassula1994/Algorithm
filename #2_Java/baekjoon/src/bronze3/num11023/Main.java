5295ba881bc32b506fdcb93a256ca4d94a375c4e
Crassula <gogun3535@gmail.com>
2024-11-03 01:32:19 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num11023/Main.java
@@ -0,0 +1,38 @@
+package bronze3.num11023;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 숫자의 합을 저장할 변수 sum 초기화
+		int sum = 0;
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// while 반복문을 사용해 숫자의 합을 갱신
+		while (st.hasMoreTokens())
+			sum += Integer.parseInt(st.nextToken());
+		
+		// valueOf() 및 write() 메서드를 사용해 숫자의 합을 출력
+		out.write(String.valueOf(sum));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
