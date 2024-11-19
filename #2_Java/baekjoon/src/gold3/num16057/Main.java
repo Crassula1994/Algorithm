5f1b5496a5b47965c0c2f6a4e4e227caa82ed2a1
Crassula1994 <gogun3535@gmail.com>
2024-11-19 09:44:58 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold3/num16057/Main.java
+++ #2_Java/baekjoon/src/gold3/num16057/Main.java
@@ -26,7 +26,7 @@ public class Main {
 		long boundaryPoints = 0;
 		
 		// 꼭짓점의 좌표를 저장할 2차원 배열 cornerPoints 초기화
-		int[][] cornerPoints = new int[pointNum + 1][2];
+		long[][] cornerPoints = new long[pointNum + 1][2];
 		
 		// for 반복문을 사용해 각 꼭짓점의 좌표를 순회
 		for (int idx = 0; idx < pointNum; idx++) {
@@ -34,9 +34,9 @@ public class Main {
 			// StringTokenizer 객체를 불러와 변수 st에 할당
 			StringTokenizer st = new StringTokenizer(in.readLine());
 			
-			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 각 꼭짓점의 좌표를 2차원 배열 cornerPoints에 저장
-			cornerPoints[idx][0] = Integer.parseInt(st.nextToken());
-			cornerPoints[idx][1] = Integer.parseInt(st.nextToken());
+			// nextToken() 및 parseLong() 메서드를 사용해 입력 받은 각 꼭짓점의 좌표를 2차원 배열 cornerPoints에 저장
+			cornerPoints[idx][0] = Long.parseLong(st.nextToken());
+			cornerPoints[idx][1] = Long.parseLong(st.nextToken());
 		}
 		
 		// 부동산의 면적과 부동산의 모서리에 위치한 정수 좌표인 점의 개수를 구하기 위해 첫 꼭짓점의 좌표를 마지막에 할당
@@ -45,12 +45,21 @@ public class Main {
 		
 		// for 반복문을 사용해 각 꼭짓점을 순회
 		for (int idx = 0; idx < pointNum; idx++) {
+			
+			// 신발끈 공식(Shoelace Formula)을 사용해 부동산의 면적을 갱신
+			area += cornerPoints[idx][0] * cornerPoints[idx + 1][1] - cornerPoints[idx][1] * cornerPoints[idx + 1][0];
+			
+			/* 부동산의 모서리에 위치한 정수 좌표의 개수는 해당 모서리의 양 꼭짓점 사이의 가로 및 세로 거리의 최대공약수와 같으므로 
+			abs() 및 gcdCalculator() 메서드를 사용해 부동산의 모서리에 위치한 정수 좌표의 개수를 갱신 */
 			boundaryPoints += gcdCalculator(Math.abs(cornerPoints[idx][0] - cornerPoints[idx + 1][0]),
 					Math.abs(cornerPoints[idx][1] - cornerPoints[idx + 1][1]));
-			area += cornerPoints[idx][0] * cornerPoints[idx + 1][1] - cornerPoints[idx][1] * cornerPoints[idx + 1][0];
 		}
 		
-		// 픽의 정리(Pick's Theorem)를 활용해
+		// abs() 메서드를 사용해 픽의 정리(Pick's Theorem)에 따라 지하수 검사를 시행할 지점의 개수를 계산하여 변수 testPoints에 할당
+		long testPoints = (Math.abs(area) - boundaryPoints + 2) / 2;
+		
+		// valueOf() 및 write() 메서드를 사용해 지하수 검사를 시행할 지점의 개수를 출력
+		out.write(String.valueOf(testPoints));
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
@@ -60,7 +69,7 @@ public class Main {
 	// ----------------------------------------------------------------------------------------------------
 	
 	// gcdCalculator() 메서드 정의
-	public static long gcdCalculator(int numberA, int numberB) {
+	public static long gcdCalculator(long numberA, long numberB) {
 		
 		// 나머지가 0인 경우 numberA가 최대공약수이므로 이를 반환
 		if (numberB == 0)
