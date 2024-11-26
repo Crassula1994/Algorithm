fa232a6334ed6c7730cff1346cc7e552ad3ed68d
Crassula1994 <gogun3535@gmail.com>
2024-11-26 15:15:18 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold4/num25330/Main.java
+++ #2_Java/baekjoon/src/gold4/num25330/Main.java
@@ -6,14 +6,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // Main 클래스 정의
 public class Main {
 	
-	// 시루가 해방시킬 수 있는 주민들의 최대 수를 저장할 변수 maxNumber 초기화
+	// 몬스터의 수, 시루가 해방시킬 수 있는 주민들의 최대 수를 저장할 각 변수 초기화
+	static int monsterNum;
 	static int maxNumber = 0;
+	
+	// 각 마을을 시루가 방문했는지 여부를 저장할 배열 visited 초기화
+	static boolean[] visited;
 	
 	// 각 마을에 있는 몬스터의 공격력 및 주민의 수를 저장할 2차원 배열 towns 초기화
 	static int[][] towns;
@@ -31,8 +34,11 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		
 		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 몬스터의 수 및 시루의 초기 체력을 각 변수에 할당
-		int monsterNum = Integer.parseInt(st.nextToken());
+		monsterNum = Integer.parseInt(st.nextToken());
 		int startEnergy = Integer.parseInt(st.nextToken());
+		
+		// 각 마을을 시루가 방문했는지 여부를 저장할 배열 visited 초기화
+		visited = new boolean[monsterNum + 1];
 		
 		// 각 마을에 있는 몬스터의 공격력 및 주민의 수를 저장할 2차원 배열 towns 초기화
 		towns = new int[monsterNum + 1][2];
@@ -51,8 +57,8 @@ public class Main {
 		for (int idx = 1; idx <= monsterNum; idx++)
 			towns[idx][1] = Integer.parseInt(st.nextToken());
 		
-		//
-		Arrays.sort(null);
+		// gameSimulator() 메서드를 호출해 시루가 해방시킬 수 있는 주민들의 최대 수를 갱신
+		gameSimulator(startEnergy, 0, 0);
 		
 		// valueOf() 및 write() 메서드를 사용해 시루가 해방시킬 수 있는 주민들의 최대 수를 출력
 		out.write(String.valueOf(maxNumber));
@@ -60,5 +66,49 @@ public class Main {
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
 		out.close();
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+	
+	// gameSimulator() 메서드 정의
+	public static void gameSimulator(int curEnergy, int residentSum, int damageSum) {
+		
+		// max() 메서드를 사용해 시루가 해방시킬 수 있는 주민들의 최대 수를 갱신
+		maxNumber = Math.max(residentSum, maxNumber);
+		
+		// 앞으로 더 해방시킬 수 있는 주민들의 수를 저장할 변수 remains 초기화
+		int remains = 0;
+		
+		// for 반복문을 사용해 각 마을을 순회
+		for (int idx = 1; idx <= monsterNum; idx++) {
+			
+			// 해당 마을을 이미 방문했거나 현재 체력으로 해당 마을을 방문할 수 없는 경우 다음 마을을 순회
+			if (visited[idx] || curEnergy < damageSum + towns[idx][0])
+				continue;
+			
+			// 앞으로 더 해방시킬 수 있는 주민들의 수를 갱신
+			remains += towns[idx][1];
+		}
+		
+		// 앞으로 더 해방시킬 수 있는 주민들의 수를 더해도 저장된 해방시킬 수 있는 주민들의 최대 수보다 적은 경우 메서드 종료
+		if (residentSum + remains <= maxNumber)
+			return;
+		
+		// for 반복문을 사용해 각 마을을 순회
+		for (int idx = 1; idx <= monsterNum; idx++) {
+			
+			// 해당 마을을 이미 방문했거나 현재 체력으로 해당 마을을 방문할 수 없는 경우 다음 마을을 순회
+			if (visited[idx] || curEnergy < damageSum + towns[idx][0])
+				continue;
+			
+			// 해당 마을을 방문 처리
+			visited[idx] = true;
+			
+			// 해당 마을을 방문한 것으로 상정해 gameSimulator() 메서드 재귀 호출
+			gameSimulator(curEnergy - damageSum - towns[idx][0], residentSum + towns[idx][1], damageSum + towns[idx][0]);
+			
+			// 해당 마을을 방문하지 않은 것으로 원상 복구
+			visited[idx] = false;
+		}
 	}
 }
