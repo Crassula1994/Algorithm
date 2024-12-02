e4f482e9bcf258a54d5dc7cc0c50c84e94c715e2
Crassula <gogun3535@gmail.com>
2024-12-02 23:47:42 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold5/num26190/Main.java
+++ #2_Java/baekjoon/src/gold5/num26190/Main.java
@@ -20,21 +20,61 @@ public class Main {
 		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 숫자의 개수를 변수 numberCount에 할당
 		int numberCount = Integer.parseInt(in.readLine());
 		
-		// 가장 작은 정규화된 값을 저장할 minValue 초기화
+		// 가장 작은 정규화된 값과 최소 오차를 저장할 각 변수 초기화
 		double minValue = Double.MAX_VALUE;
+		double minError = Double.MAX_VALUE;
 		
-		// 정규화된 숫자 목록을 저장할 배열 normalizedNumbers 초기화
-		double[] normalizedNumbers = new double[numberCount];
+		// 정규화된 숫자 목록과 원래 숫자 목록을 저장할 각 배열 초기화
+		double[] normalizedList = new double[numberCount];
+		long[] denormalizedList = new long[numberCount];
 		
 		// for 반복문을 사용해 각 정규화된 값을 순회
-		for (int idx = 0; idx < normalizedNumbers.length; idx++) {
+		for (int idx = 0; idx < normalizedList.length; idx++) {
 			
-			// readLine() 및 parseDouble() 메서드를 사용해 입력 받은 정규화된 숫자를 배열 normalizedNumbers에 저장
-			normalizedNumbers[idx] = Double.parseDouble(in.readLine());
+			// readLine() 및 parseDouble() 메서드를 사용해 입력 받은 정규화된 숫자를 배열 normalizedList에 저장
+			normalizedList[idx] = Double.parseDouble(in.readLine());
 			
 			// min() 메서드를 사용해 가장 작은 정규화된 값을 갱신
-			minValue = Math.min(normalizedNumbers[idx], minValue);
+			minValue = Math.min(normalizedList[idx], minValue);
 		}
+		
+		// for 반복문을 사용해 각 정규화된 값을 1 이상으로 조정
+		for (int idx = 0; idx < normalizedList.length; idx++)
+			normalizedList[idx] /= minValue;
+
+		// for 반복문을 사용해 가능한 각 정규화 상수를 순회
+		for (int scale = 1; scale <= 10000; scale++) {
+			
+			// 현재 정규화 상수에서의 오차를 저장할 변수 curError 초기화
+			double curError = 0;
+			
+			// 현재 정규화 상수에서 복원해 반올림한 값을 저장할 배열 scaledList 초기화
+			long[] scaledList = new long[numberCount];
+
+			// for 반복문을 사용해 각 숫자를 순회
+			for (int idx = 0; idx < normalizedList.length; idx++) {
+				
+				// round() 메서드를 사용해 복원한 값과 이를 반올림한 값을 각 변수에 할당
+				double scaledValue = normalizedList[idx] * scale;
+				long roundedValue = Math.round(scaledValue);
+				
+				// 현재 정규화 상수에서 복원해 반올림한 값을 배열 scaledList에 저장
+				scaledList[idx] = roundedValue;
+				
+				// abs() 및 max() 메서드를 사용해 현재 정규화 상수에서의 오차를 갱신
+				curError = Math.max(Math.abs(roundedValue - scaledValue), curError);
+			}
+			
+			// 현재 정규화 상수에서의 오차가 저장된 오차보다 더 적은 경우 최소 오차와 원래 숫자 목록을 갱신
+			if (curError < minError) {
+				minError = curError;
+				denormalizedList = scaledList;
+			}
+		}
+		
+		// for 반복문을 사용해 원래 숫자 목록을 출력
+		for (long value : denormalizedList)
+			out.write(value + "\n");
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
