04753f20948d220849a7c178d8a079908e866152
Crassula1994 <gogun3535@gmail.com>
2024-12-27 10:10:04 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze2/num21756/Main.java
@@ -0,0 +1,37 @@
+package bronze2.num21756;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 칸의 개수를 변수 cellNum에 할당
+		int cellNum = Integer.parseInt(in.readLine());
+		
+		// 마지막에 남게 되는 숫자를 저장할 변수 lastNum 초기화
+		int lastNum = 1;
+		
+		// while 반복문을 사용해 마지막에 남게 되는 숫자를 갱신
+		while (lastNum * 2 <= cellNum)
+			lastNum *= 2;
+		
+		// valueOf() 및 write() 메서드를 사용해 마지막에 남게 되는 숫자를 출력
+		out.write(String.valueOf(lastNum));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
