a691ef506c731dd8cfe45242d3c0867eabadf25d
Crassula1994 <gogun3535@gmail.com>
2025-01-15 18:47:04 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/gold5/num03079/Main.java
@@ -0,0 +1,39 @@
+package gold5.num03079;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// StringTokenizer 객체를 불러와 변수 st에 할당
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 입국심사대의 개수 및 심사를 받아야 할 사람의 수를 각 변수에 할당
+		int checkpointNum = Integer.parseInt(st.nextToken());
+		int peopleNum = Integer.parseInt(st.nextToken());
+		
+		// 각 입국심사대에서 심사를 하는 데 걸리는 시간을 저장할 배열 checkpoints 초기화
+		int[] checkpoints = new int[checkpointNum];
+		
+		// for 반복문을 사용해 입력 받은 각 입국심사대에서 심사를 하는 데 걸리는 시간을 배열 checkpoints에 저장
+		for (int idx = 0; idx < checkpoints.length; idx++)
+			checkpoints[idx] = Integer.parseInt(in.readLine());
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
