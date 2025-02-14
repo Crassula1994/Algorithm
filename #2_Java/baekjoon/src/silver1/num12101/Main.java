2380c8cdc78753f62dc13a1331d7ba95568000eb
Crassula1994 <gogun3535@gmail.com>
2025-02-14 10:28:58 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/silver1/num12101/Main.java
+++ #2_Java/baekjoon/src/silver1/num12101/Main.java
@@ -14,7 +14,7 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	// 목표 숫자와 사전 순서, 지금까지 찾은 방법의 수를 저장할 각 변수 초기화
-	static int number;
+	static int targetNumber;
 	static int targetOrder;
 	static int count = 0;
 	
@@ -34,7 +34,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		
 		// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 목표 숫자와 사전 순서를 각 변수에 할당
-		number = Integer.parseInt(st.nextToken());
+		targetNumber = Integer.parseInt(st.nextToken());
 		targetOrder = Integer.parseInt(st.nextToken());
 		
 		// methodFinder() 메서드를 호출해 찾고자 하는 사전 순서에 해당하는 방법을 갱신
@@ -48,9 +48,15 @@ public class Main {
 		} else {
 			
 			// for 반복문을 사용해 해당 방법의 각 숫자를 순회
-			for (int idx = 0; idx < method.size(); idx++) {}
-			
-			
+			for (int idx = 0; idx < method.size(); idx++) {
+				
+				// get(), valueOf(), write() 메서드를 사용해 해당 숫자를 출력
+				out.write(String.valueOf(method.get(idx)));
+				
+				// 마지막 숫자가 아닌 경우 덧셈 기호를 출력
+				if (idx < method.size() - 1)
+					out.write("+");
+			}
 		}
 		
 		// close() 메서드를 사용해 각 객체 종료
@@ -63,5 +69,31 @@ public class Main {
 	// methodFinder() 메서드 정의
 	public static void methodFinder(int sum, int curIdx) {
 		
+		// 합이 목표 숫자를 만든 경우 찾은 방법의 수를 갱신 후 메서드 종료
+		if (sum == targetNumber) {
+			count++;
+			return;
+		}
+		
+		// for 반복문을 사용해 더할 숫자를 차례로 순회
+		for (int number = 1; number <= 3; number++) {
+			
+			// 숫자를 더했을 때 목표 숫자를 초과하는 경우 반복문 탈출
+			if (sum + number > targetNumber)
+				break;
+			
+			// add() 메서드를 사용해 해당 숫자를 method에 추가
+			method.add(number);
+			
+			// methodFinder() 메서드를 재귀 호출
+			methodFinder(sum + number, curIdx + 1);
+			
+			// 찾고자 하는 사전 순서에 도달한 경우 반복문 탈출
+			if (count == targetOrder)
+				break;
+			
+			// remove() 메서드를 사용해 해당 숫자를 method에서 제거
+			method.remove(curIdx);
+		}
 	}
 }
