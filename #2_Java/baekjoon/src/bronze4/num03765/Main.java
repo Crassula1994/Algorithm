2847fb99c3a842ee3133e2852ef4aadbe2e3caab
Crassula <gogun3535@gmail.com>
2025-02-22 14:23:01 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num03765/Main.java
@@ -0,0 +1,37 @@
+package bronze4.num03765;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 주어진 방정식을 저장할 변수 equation 초기화
+		String equation;
+		
+		// while 반복문을 사용해 각 방정식을 순회
+		while ((equation = in.readLine()) != null) {
+			
+			// write() 메서드를 사용해 해당 방정식을 출력
+			out.write(equation);
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
