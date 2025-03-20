6c75f5f778744a689d14cae94dec4a3c9bd4e574
Crassula1994 <gogun3535@gmail.com>
2025-03-20 19:00:39 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze1/num09494/Main.java
@@ -0,0 +1,32 @@
+package bronze1.num09494;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// while 반복문을 사용해 각 테스트 케이스를 순회
+		while (true) {
+			
+			// readLine() 및 parseInt() 메서드를 사용해 텍스트의
+			
+			
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
