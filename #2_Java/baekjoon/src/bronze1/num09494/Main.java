f07e0ae453340c1fb32c9fa0be0c8c89c3b00680
Crassula <gogun3535@gmail.com>
2025-03-20 23:26:49 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/bronze1/num09494/Main.java
+++ #2_Java/baekjoon/src/bronze1/num09494/Main.java
@@ -20,9 +20,37 @@ public class Main {
 		// while 반복문을 사용해 각 테스트 케이스를 순회
 		while (true) {
 			
-			// readLine() 및 parseInt() 메서드를 사용해 텍스트의
+			// readLine() 및 parseInt() 메서드를 사용해 입력 받은 글의 줄 수를 변수 lineNum에 할당
+			int lineNum = Integer.parseInt(in.readLine());
 			
+			// 모든 테스트 케이스를 확인한 경우 반복문 탈출
+			if (lineNum == 0)
+				break;
 			
+			// 공이 떨어지는 세로단의 위치를 저장할 변수 dropPoint 초기화
+			int dropPoint = 0;
+			
+			// for 반복문을 사용해 각 줄을 순회
+			for (int line = 1; line <= lineNum; line++) {
+				
+				// readLine() 메서드를 사용해 글의 해당 줄을 변수 textLine에 할당
+				String textLine = in.readLine();
+				
+				// 첫 번째 줄인 경우
+				if (line == 1) {
+					
+					// while 반복문을 사용해 공이 떨어지는 세로단의 위치를 갱신
+					while (textLine.charAt(dropPoint) == ' ')
+						dropPoint++;
+				}
+				
+				// while 반복문을 사용해 공이 떨어지는 세로단의 위치를 갱신
+				while (dropPoint < textLine.length() && textLine.charAt(dropPoint) != ' ')
+					dropPoint++;
+			}
+			
+			// write() 메서드를 사용해 공이 떨어지는 세로단의 위치를 출력
+			out.write(++dropPoint + "\n");
 		}
 		
 		// close() 메서드를 사용해 각 객체 종료
