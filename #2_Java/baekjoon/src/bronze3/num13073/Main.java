556d67d422939cc5055b8600663b0346477fe5ad
Crassula1994 <gogun3535@gmail.com>
2025-04-02 11:21:33 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num13073/Main.java
@@ -0,0 +1,42 @@
+package bronze3.num13073;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;  
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 개수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 테스트 케이스를 순회
+		while (testCase-- > 0) {
+			
+			// readLine() 및 parseInt() 메서드를 사용해 입력 받은 숫자를 변수 number에 할당
+			int number = Integer.parseInt(in.readLine());
+			
+			// 입력 받은 숫자만큼의 정수를 더한 합, 홀수를 더한 합, 짝수를 더한 합을 계산해 각 변수에 할당
+			int sum = number * (number + 1) / 2;
+			int oddSum = number * number;
+			int evenSum = sum * 2;
+			
+			// write() 메서드를 사용해 입력 받은 숫자만큼의 정수를 더한 합, 홀수를 더한 합, 짝수를 더한 합을 출력
+			out.write(sum + " " + oddSum + " " + evenSum + "\n");
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
