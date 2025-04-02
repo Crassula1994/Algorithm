15bd16bc1c370f289158dd44be6d6c2cd44abbd8
Crassula <gogun3535@gmail.com>
2025-04-02 23:14:17 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/bronze2/num16171/Main.java
+++ #2_Java/baekjoon/src/bronze2/num16171/Main.java
@@ -17,9 +17,9 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		// readLine() 메서드를 사용해 입력 받은 교과서의 문자열과 키워드의 문자열의 각 변수에 할당
+		// readLine() 메서드를 사용해 입력 받은 교과서의 문자열과 키워드의 문자열을 각 변수에 할당
 		String textString = in.readLine();
-		String keywordString = in.readLine();
+		String keyword = in.readLine();
 		
 		// 숫자를 제거한 교과서의 문자열을 저장할 StringBuilder 객체 sb 초기화
 		StringBuilder sb = new StringBuilder();
@@ -27,7 +27,7 @@ public class Main {
 		// for 반복문을 사용해 교과서 문자열의 각 문자를 순회
 		for (int idx = 0; idx < textString.length(); idx++) {
 			
-			// charAt() 메서드를 사용해 입력 받은 문자를 변수 character에 할당
+			// charAt() 메서드를 사용해 교과서 문자열의 해당 문자를 변수 character에 할당
 			char character = textString.charAt(idx);
 			
 			// 해당 문자가 숫자인 경우 다음 문자를 순회
@@ -39,7 +39,7 @@ public class Main {
 		}
 		
 		// toString(), contains(), write() 메서드를 사용해 성민이가 찾고자 하는 키워드가 문자열 내에 존재하는지 여부를 출력
-		out.write((sb.toString().contains(keywordString)) ? "1" : "0");
+		out.write((sb.toString().contains(keyword)) ? "1" : "0");
 		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
