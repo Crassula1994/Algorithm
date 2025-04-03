208aa94a55db5423de5ba2f7ebd688b513542e74
Crassula1994 <gogun3535@gmail.com>
2025-04-03 11:28:27 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num31610/Main.java
@@ -0,0 +1,32 @@
+package bronze5.num31610;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 알사탕의 가격, 알사탕의 개수, 봉투의 가격을 각 변수에 할당
+		int candyPrice = Integer.parseInt(in.readLine());
+		int candyCount = Integer.parseInt(in.readLine());
+		int sackPrice = Integer.parseInt(in.readLine());
+		
+		// valueOf() 및 write() 메서드를 사용해 합계 금액을 출력
+		out.write(String.valueOf(candyPrice * candyCount + sackPrice));
+
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
