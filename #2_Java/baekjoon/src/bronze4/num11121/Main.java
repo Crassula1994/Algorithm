393967f342cd01f7e228c37547d9b55ec93f9e67
Crassula1994 <gogun3535@gmail.com>
2025-04-03 10:03:33 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze4/num11121/Main.java
@@ -0,0 +1,41 @@
+package bronze4.num11121;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 전송 횟수를 변수 transmissionNum에 할당
+		int transmissionNum = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 전송 횟수를 순회
+		while (transmissionNum-- > 0) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// nextToken(), equals(), write() 메서드를 사용해 전송한 내용이 정확한지 여부를 출력
+			out.write((st.nextToken().equals(st.nextToken())) ? "OK" : "ERROR");
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
