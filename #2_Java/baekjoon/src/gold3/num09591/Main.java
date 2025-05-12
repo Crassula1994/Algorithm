993a5e1f1ac75be5dd20692ecb27fc58ff94af7b
Crassula <gogun3535@gmail.com>
2025-05-13 08:02:36 +0900

    Baekjoon Java Solution Modified

--- #2_Java/baekjoon/src/gold3/num09591/Main.java
+++ #2_Java/baekjoon/src/gold3/num09591/Main.java
@@ -43,8 +43,64 @@ public class Main {
 		int placeNum = Integer.parseInt(st.nextToken());
 		int connectionNum = Integer.parseInt(st.nextToken());
 		
+		// 입력 받은 장소의 정보를 저장할 배열 places 초기화
+		Place[] places = new Place[placeNum];
+		
+		// 각 장소 사이의 거리와 최단거리를 저장할 각 2차원 배열 초기화
+		double[][] distances = new double[placeNum][placeNum];
+		double[][] minDistances = new double[placeNum][placeNum];
+		
+		// for 반복문을 사용해 각 장소의 정보를 순회
+		for (int idx = 0; idx < places.length; idx++) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 재할당
+			st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 해당 장소의 층수와 각 좌표를 각 변수에 할당
+			int floor = Integer.parseInt(st.nextToken());
+			int coordinateX = Integer.parseInt(st.nextToken());
+			int coordinateY = Integer.parseInt(st.nextToken());
+			
+			// 해당 장소의 정보를 배열 places에 저장
+			places[idx] = new Place(floor, coordinateX, coordinateY);
+		}
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 질의의 수를 변수 queryNum에 할당
+		int queryNum = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 질의를 순회
+		while (queryNum-- > 0) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 재할당
+			st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 두 장소의 번호를 각 변수에 할당
+			int origin = Integer.parseInt(st.nextToken());
+			int destination = Integer.parseInt(st.nextToken());
+			
+			// shortestPathFinder() 및 write() 메서드를 사용해 출발 장소에서 도착 장소까지 최단 거리로 가기 위한 경로를 출력
+			out.write(shortestPathFinder(origin, destination));
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
 		// close() 메서드를 사용해 각 객체 종료
 		in.close();
 		out.close();
+	}
+	
+	// ----------------------------------------------------------------------------------------------------
+
+	// shortestPathFinder() 메서드 정의
+	public static String shortestPathFinder(int origin, int destination) {
+		
+		// 출발 장소에서 도착 장소까지 최단 거리로 가기 위한 경로를 저장할 StringBuilder 객체 sb 초기화
+		StringBuilder sb = new StringBuilder();
+		
+		
+		
+		// toString() 메서드를 사용해 출발 장소에서 도착 장소까지 최단 거리로 가기 위한 경로를 반환
+		return sb.toString();
 	}
 }
