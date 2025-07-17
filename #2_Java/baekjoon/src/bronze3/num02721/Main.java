06b5282eb47b7be1010bcacd555b8f3b2781211a
Crassula1994 <gogun3535@gmail.com>
2025-07-17 10:13:30 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num02721/Main.java
@@ -0,0 +1,38 @@
+package bronze3.num02721;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 각 삼각수의 합을 저장할 배열 sums 초기화
+		int[] sums = new int[301];
+		
+		// for 반복문을 사용해 각 삼각수의 합을 갱신
+		for (int idx = 1; idx < sums.length; idx++)
+			sums[idx] = sums[idx - 1] + (idx + 1) * (idx + 2) / 2 * idx;
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 개수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 테스트 케이스에 대하여 삼각수의 합을 출력
+		while (testCase-- > 0)
+			out.write(sums[Integer.parseInt(in.readLine())] + "\n");
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
