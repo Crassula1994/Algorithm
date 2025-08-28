c3d2d1d8425002e7b63bdfbefe487c2534f49a6f
Crassula1994 <gogun3535@gmail.com>
2025-08-28 09:37:36 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze2/num05026/Main.java
@@ -0,0 +1,52 @@
+package bronze2.num05026;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 문제의 수를 변수 questionNum에 할당
+		int questionNum = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 문제를 순회
+		while (questionNum-- > 0) {
+			
+			// readLine() 메서드를 사용해 입력 받은 해당 문제를 변수 question에 할당
+			String question = in.readLine();
+			
+			// 해당 문제가 컴퓨터 과학 문제인 경우 'skipped'를 출력
+			if (question.equals("P=NP")) {
+				out.write("skipped");
+				
+			// 해당 문제가 덧셈 문제인 경우
+			} else {
+				
+				// StringTokenizer 객체를 불러와 변수 st에 할당
+				StringTokenizer st = new StringTokenizer(question, "+");
+				
+				// nextToken(), parseInt(), valueOf(), write() 메서드를 사용해 덧셈한 결과를 출력
+				out.write(String.valueOf(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+			}
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
