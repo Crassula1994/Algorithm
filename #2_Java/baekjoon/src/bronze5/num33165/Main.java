e79e1dbc192cf312d98d4666163e2db9641bf5a0
Crassula1994 <gogun3535@gmail.com>
2025-09-27 21:06:50 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze5/num33165/Main.java
@@ -0,0 +1,31 @@
+package bronze5.num33165;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 비타로가 달린 시간 및 속도를 각 변수에 할당
+		int time = Integer.parseInt(in.readLine());
+		int velocity = Integer.parseInt(in.readLine());
+		
+		// valueOf() 및 write() 메서드를 사용해 비타로가 달린 거리를 출력
+		out.write(String.valueOf(time * velocity));
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
