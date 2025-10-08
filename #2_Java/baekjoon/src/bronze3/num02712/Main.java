49ad2f08b5593db39807a49e09c749658d9bc3d2
Crassula1994 <gogun3535@gmail.com>
2025-10-08 23:31:03 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num02712/Main.java
@@ -0,0 +1,57 @@
+package bronze3.num02712;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// readLine() 및 parseInt() 메서드를 사용해 입력 받은 테스트 케이스의 개수를 변수 testCase에 할당
+		int testCase = Integer.parseInt(in.readLine());
+		
+		// while 반복문을 사용해 각 테스트 케이스를 순회
+		while (testCase-- > 0) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseDouble() 메서드를 사용해 입력 받은 값과 단위를 각 변수에 할당
+			double value = Double.parseDouble(st.nextToken());
+			String unit = st.nextToken();
+			
+			// switch 조건문을 사용해 각 단위를 변환한 결과를 출력
+			switch (unit) {
+				case "kg":
+					out.write(String.format("%.4f lb", value * 2.2046));
+					break;
+				case "lb":
+					out.write(String.format("%.4f kg", value * 0.4536));
+					break;
+				case "l":
+					out.write(String.format("%.4f g", value * 0.2642));
+					break;
+				default:
+					out.write(String.format("%.4f l", value * 3.7854));
+			}
+			
+			// newLine() 메서드를 사용해 줄바꿈을 출력
+			out.newLine();
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
