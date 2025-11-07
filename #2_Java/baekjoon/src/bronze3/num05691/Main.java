d06f008fde9d5ea1941af40e9f15dabe4e4b8a3f
Crassula <gogun3535@gmail.com>
2025-11-07 23:15:23 +0900

    Baekjoon Java Solution Added

---
+++ #2_Java/baekjoon/src/bronze3/num05691/Main.java
@@ -0,0 +1,43 @@
+package bronze3.num05691;
+
+// 필요한 패키지 불러오기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// Main 클래스 정의
+public class Main {
+	
+	// main() 메서드 정의
+	public static void main(String[] args) throws IOException {
+		
+		// BufferedReader 및 BufferedWriter 객체를 불러와 각 변수에 할당
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// while 반복문을 사용해 각 테스트 케이스를 순회
+		while (true) {
+			
+			// StringTokenizer 객체를 불러와 변수 st에 할당
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			// nextToken() 및 parseInt() 메서드를 사용해 입력 받은 두 정수를 변수 각 변수에 할당
+			int numberA = Integer.parseInt(st.nextToken());
+			int numberB = Integer.parseInt(st.nextToken());
+			
+			// 모든 테스트 케이스를 확인한 경우 반복문 탈출
+			if (numberA == 0 && numberB == 0)
+				break;
+			
+			// write() 메서드를 사용해 평균과 중앙값을 같게 만드는 가장 작은 정수를 출력
+			out.write((numberA - (numberB - numberA)) + "\n");
+		}
+		
+		// close() 메서드를 사용해 각 객체 종료
+		in.close();
+		out.close();
+	}
+}
